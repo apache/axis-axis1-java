afbecde2324976dcbdff67a2216a33903a258267
neyama <dev-null@apache.org>
2001-01-30 08:10:29 +0000

    Added README file for TRL Proposal. Fixed coding style including indentation in the source code. PR: Obtained from: Submitted by: Reviewed by:

--- prototype/TRL_Proposal/org/apache/axis/SOAPFaultException.java
+++ prototype/TRL_Proposal/org/apache/axis/SOAPFaultException.java
@@ -3,20 +3,20 @@ package org.apache.axis;
 import org.apache.axis.SOAPException;
 
 public class SOAPFaultException extends SOAPException {
-    MessageContext faultMessage ;
+    MessageContext faultMessage;
     public SOAPFaultException() { }
 
     public SOAPFaultException(MessageContext faultMessage) {
-        super("A SOAP Fault occured") ;
-        setFaultMessage(faultMessage) ;
+        super("A SOAP Fault occured");
+        setFaultMessage(faultMessage);
     }
 
     public void setFaultMessage(MessageContext faultMessage) {
-        this.faultMessage = faultMessage ;
+        this.faultMessage = faultMessage;
     }
-    public MessageContext getFaultMessage() { return faultMessage ; }
+    public MessageContext getFaultMessage() { return faultMessage; }
 
     public String toString() {
-        return (super.toString() + "\nThe SOAP Fault message contained in the exception is as follows:\n" + faultMessage) ;
+        return (super.toString() + "\nThe SOAP Fault message contained in the exception is as follows:\n" + faultMessage);
     }
 }
