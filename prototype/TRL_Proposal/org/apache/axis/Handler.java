878ad7f6c3c9d048cc832e359ef9a3d618fd9955
nakamury <dev-null@apache.org>
2001-01-30 07:09:29 +0000

    Added Handler, and ChainContainer (Single Chain Impl.)

---
+++ prototype/TRL_Proposal/org/apache/axis/Handler.java
@@ -0,0 +1,28 @@
+package org.apache.axis;
+
+import java.io.Serializable;
+
+  /**
+   * We may have to remove Serializable because we need 
+   * onSerialize and onDederialize.
+   */
+
+public interface Handler extends Serializable {
+  /**
+   * Init is called when the chain containing this Chainable object
+   * is instantiated.
+   */
+  void init();
+
+  /**
+   * Cleanup is called when the chain containing this Chainable object
+   * is done processing the chain.
+   */
+  void cleanup();
+
+  /**
+   * Invoke is called to do the actual work of the Chainable object.
+   */
+  void invoke(MessageContext message);
+
+};
