1338d0774ab6ed5e64fa00819cad1e39e5b06fcf
Doug Davis <dug@apache.org>
2001-02-09 12:17:38 +0000

    changed the auth files to have more generic users. Added a couple of testit scripts to help verify that we don't break anything.

---
+++ samples/stock/testit.sh
@@ -0,0 +1,20 @@
+#this assumes webserver is running on port 8080
+#this assumes that the samples/xml/init.xml file has already been deployed
+
+echo Deploy everything first
+java org.apache.axis.client.AdminClient deploy.xml
+
+echo These next 2 should work...
+java samples.stock.GetQuote -uuser1 -wpass1 XXX
+java samples.stock.GetQuote -uuser2 XXX
+
+echo The rest of these should fail... (nicely of course)
+java samples.stock.GetQuote XXX
+java samples.stock.GetQuote -uuser1 -wpass2 XXX
+java samples.stock.GetQuote -uuser3 -wpass3 XXX
+
+echo This should work but print debug info on the client and server
+java samples.stock.GetQuote -d -uuser1 -wpass1 XXX
+
+rem Now undeploy everything
+java org.apache.axis.client.AdminClient undeploy.xml
