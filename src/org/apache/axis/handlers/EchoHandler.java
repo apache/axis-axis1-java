5c086924c842375a08a1bc9cec984c35e80bd094
James M. Snell <jmsnell@apache.org>
2001-03-26 21:12:39 +0000

    no message

--- src/org/apache/axis/handlers/EchoHandler.java
+++ src/org/apache/axis/handlers/EchoHandler.java
@@ -57,7 +57,11 @@ package org.apache.axis.handlers ;
 
 import org.apache.axis.* ;
 import org.apache.axis.utils.* ;
-import org.apache.axis.message.* ;
+import org.apache.axis.message.RPCArg;
+import org.apache.axis.message.RPCBody;
+import org.apache.axis.message.SOAPBody;
+import org.apache.axis.message.SOAPEnvelope;
+import org.apache.axis.message.SOAPHeader;
 
 /**
  *
