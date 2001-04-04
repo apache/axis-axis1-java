e12010ebf1fcdafaf592484db2898001ee673952
neyama <dev-null@apache.org>
2001-04-04 06:27:21 +0000

    Fixed some syntax errors in the shell script. PR: Obtained from: Submitted by: Reviewed by:

--- samples/stock/testit.sh
+++ samples/stock/testit.sh
@@ -1,20 +1,21 @@
-#this assumes webserver is running on port 8080
-#this assumes that the samples/xml/init.xml file has already been deployed
+#!/bin/sh
+# this assumes webserver is running on port 8080
+# this assumes that the samples/xml/init.xml file has already been deployed
 
-echo Deploy everything first
+echo "Deploy everything first"
 java org.apache.axis.client.AdminClient deploy.xml
 
-echo These next 2 should work...
+echo "These next 2 should work..."
 java samples.stock.GetQuote -uuser1 -wpass1 XXX
 java samples.stock.GetQuote -uuser2 XXX
 
-echo The rest of these should fail... (nicely of course)
+echo "The rest of these should fail... (nicely of course)"
 java samples.stock.GetQuote XXX
 java samples.stock.GetQuote -uuser1 -wpass2 XXX
 java samples.stock.GetQuote -uuser3 -wpass3 XXX
 
-echo This should work but print debug info on the client and server
+echo "This should work but print debug info on the client and server"
 java samples.stock.GetQuote -d -uuser1 -wpass1 XXX
 
-rem Now undeploy everything
+# Now undeploy everything
 java org.apache.axis.client.AdminClient undeploy.xml
