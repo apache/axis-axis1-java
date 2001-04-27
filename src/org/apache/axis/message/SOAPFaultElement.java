6503a95f339d8d35105fb9fcb1a090239e392b6c
Sam Ruby <rubys@apache.org>
2001-04-27 19:58:29 +0000

    Oops!  Check in the rest of the change...

---
+++ src/org/apache/axis/message/SOAPFaultElement.java
@@ -0,0 +1,160 @@
+package org.apache.axis.message;
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Axis" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import org.w3c.dom.Element;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+import org.apache.axis.AxisFault;
+import org.apache.axis.Constants;
+import org.apache.axis.encoding.*;
+import org.apache.axis.utils.QName;
+import java.util.*;
+import java.io.*;
+
+/** An Fault body element.
+ * 
+ * @author Sam Ruby (rubys@us.ibm.com)
+ * @author Glen Daniels (gdaniels@macromedia.com)
+ */
+public class SOAPFaultElement extends SOAPBodyElement
+{
+    static class FaultElementFactory implements ElementFactory
+    {
+        public MessageElement createElement(String namespace, String localName,
+                                        Attributes attributes, DeserializationContext context)
+        {
+            return new SOAPFaultElement(namespace, localName, attributes, context);
+        }
+    }
+    
+    public static ElementFactory getFactory()
+    {
+        return new FaultElementFactory();
+    }
+    
+    class FaultContentHandler extends DefaultHandler
+    {
+        private boolean passedMyStart = false;
+        
+        public void startElement(String namespace, String name, String qName,
+                                 Attributes attributes)
+        {
+            // *** TBD ***
+        }
+    }
+    public ContentHandler getContentHandler() { return new FaultContentHandler(); }
+    
+    ///////////////////////////////////////////////////////////////
+    
+    protected AxisFault fault;    
+    
+    public SOAPFaultElement(String namespace, String localName, Attributes attrs,
+                      DeserializationContext context)
+    {
+        super(namespace, localName, attrs, context);
+    }
+    
+    public SOAPFaultElement(AxisFault fault)
+    {
+        this.fault = fault;
+    }
+    
+    public void output(SerializationContext context)
+        throws IOException
+    {
+        context.registerPrefixForURI(prefix, namespaceURI);
+        context.startElement(new QName(this.getNamespaceURI(), this.getName()), attributes);
+
+        if (fault.getFaultCode() != null) {
+          MessageElement element = new 
+            MessageElement(Constants.URI_SOAP_ENV, "faultcode", null, null);
+          element.setValue(fault.getFaultCode());
+          element.output(context);
+        }
+    
+        if (fault.getFaultString() != null) {
+          MessageElement element = new 
+            MessageElement(Constants.URI_SOAP_ENV, "faultstring", null, null);
+          element.setValue(fault.getFaultString());
+          element.output(context);
+        }
+    
+        if (fault.getFaultActor() != null) {
+          MessageElement element = new 
+            MessageElement(Constants.URI_SOAP_ENV, "faultactor", null, null);
+          element.setValue(fault.getFaultActor());
+          element.output(context);
+        }
+    
+        Element[] faultDetails = fault.getFaultDetails();
+        if (faultDetails != null) {
+          //*** TBD ***
+        }
+    
+        context.endElement();
+    }
+    
+    public AxisFault getAxisFault()
+    {
+        return fault;
+    }
+    
+    public void setAxisFault(AxisFault fault)
+    {
+        this.fault = fault;
+    }
+}
