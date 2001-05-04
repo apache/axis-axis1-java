ed40ff2e291cfd0652fa2b7b1f469ca2c71745be
Glen Daniels <gdaniels@apache.org>
2001-05-04 19:27:52 +0000

    Clean up a bit, and support for ID/HREFs.

--- samples/encoding/Data.java
+++ samples/encoding/Data.java
@@ -4,4 +4,26 @@ public class Data
 {
     public String stringMember;
     public Float floatMember;
+    public Data dataMember;
+    
+    public String toString()
+    {
+        return getStringVal("");
+    }
+    
+    public String getStringVal(String indent)
+    {
+        String ret = "\n" + indent + "Data:\n";
+        ret +=       indent + " str[" + stringMember + "]\n";
+        ret +=       indent + " float[" + floatMember + "]\n";
+        ret +=       indent + " data[";
+        
+        if (dataMember != null)
+            ret += dataMember.getStringVal(indent + "  ") + "\n" + indent;
+        else
+            ret += " null";
+        
+        ret += " ]";
+        return ret;
+    }
 }
