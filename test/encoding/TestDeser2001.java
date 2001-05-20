978db0223e6044a541918b6dd9022ee235d20223
Sam Ruby <rubys@apache.org>
2001-05-20 22:14:06 +0000

    Correctly handle times past noon.

--- test/encoding/TestDeser2001.java
+++ test/encoding/TestDeser2001.java
@@ -2,6 +2,8 @@ package test.encoding;
 
 import org.apache.axis.Constants;
 import junit.framework.TestCase;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /** 
  * Test deserialization of SOAP responses
@@ -12,4 +14,19 @@ public class TestDeser2001 extends TestDeser {
         super(name, Constants.URI_2001_SCHEMA_XSI, 
                     Constants.URI_2001_SCHEMA_XSD);
     }
+
+    /** 
+     * Test deserialization of Date responses
+     */
+    public void testDate() {
+        Calendar date = Calendar.getInstance();
+        date.set(1999,04,31,12,01,30);
+        date.setTimeZone(TimeZone.getTimeZone("GMT"));
+        date.set(Calendar.MILLISECOND,150);
+        deserialize("<result xsi:type=\"xsd:dateTime\">" + 
+                       "1999-05-31T12:01:30.150Z" + 
+                     "</result>",
+                     date.getTime());
+    }
+
 }
