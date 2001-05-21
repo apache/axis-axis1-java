fd10d818e04e8cad52f2ec4e5a13bddf533666eb
Sam Ruby <rubys@apache.org>
2001-05-21 19:14:50 +0000

    Add a simple unit test (hopefully the first of many) for RPCDispatch

---
+++ test/RPCDispatch/PackageTests.java
@@ -0,0 +1,23 @@
+package test.RPCDispatch;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * RPCDispatch's PackageTests tests
+ */
+public class PackageTests extends TestCase {
+
+    public PackageTests(String name) {
+        super(name);
+    }
+
+    public static Test suite() throws Exception {
+        TestSuite suite = new TestSuite();
+
+        suite.addTestSuite(TestRPC.class);
+
+        return suite;
+    }
+}
