0278aff63659dc2ee27c39cf4a85724a29aeff92
Glen Daniels <gdaniels@apache.org>
2001-06-12 22:26:54 +0000

    Trivial tests for Session.  Will improve later.

---
+++ test/session/PackageTests.java
@@ -0,0 +1,23 @@
+package test.session;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Session tests
+ */
+public class PackageTests extends TestCase {
+
+    public PackageTests(String name) {
+        super(name);
+    }
+
+    public static Test suite() throws Exception {
+        TestSuite suite = new TestSuite();
+
+        suite.addTestSuite(TestSimpleSession.class);
+
+        return suite;
+    }
+}
---
+++ test/session/TestSimpleSession.java
@@ -0,0 +1,34 @@
+package test.session;
+
+import org.apache.axis.session.SimpleSession;
+
+import junit.framework.TestCase;
+
+/** 
+ * Test deserialization of SOAP responses
+ */
+public class TestSimpleSession extends TestCase {
+    public TestSimpleSession(String name)
+    {
+        super(name);
+    }
+    
+    public void testSession() {
+        SimpleSession session = new SimpleSession();
+        Object val = new Float(5.6666);
+        session.put("test", val);
+        
+        assertEquals(val, session.get("test"));
+        
+        session.remove("test");
+        
+        assertNull(session.get("test"));
+        
+    }
+    
+    public static void main(String args[])
+    {
+        TestSimpleSession test = new TestSimpleSession("test");
+        test.testSession();
+    }
+}
