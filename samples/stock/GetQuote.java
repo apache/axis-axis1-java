af78751db8cb32100a1a9c2a646d0eb25eab1dd3
Glen Daniels <gdaniels@apache.org>
2001-06-14 16:31:02 +0000

    Don't bother printing a stack trace if it's an AxisFault, just dump it.
    We might want to alter dump() to know if it's a "local" or "remote"
    fault, and print the stack trace if it's local.  If it's remote, we know
    precisely where the fault will always appear to come from (i.e.
    SOAPFaultElement.java:110).

--- samples/stock/GetQuote.java
+++ samples/stock/GetQuote.java
@@ -135,8 +135,10 @@ public class GetQuote {
         System.out.println(gq.symbol + ": " + val);
     }
     catch( Exception e ) {
-      if ( e instanceof AxisFault ) ((AxisFault)e).dump();
-      e.printStackTrace();
+        if ( e instanceof AxisFault ) {
+            ((AxisFault)e).dump();
+        } else
+            e.printStackTrace();
     }
   }
   
