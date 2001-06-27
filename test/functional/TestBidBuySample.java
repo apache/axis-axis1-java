5e82bd22f164ca3c170c64a0bd86465cb4947ff9
Glen Daniels <gdaniels@apache.org>
2001-06-27 04:45:19 +0000

    Fix AdminClient usage to match current state.

--- test/functional/TestBidBuySample.java
+++ test/functional/TestBidBuySample.java
@@ -2,7 +2,7 @@
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -60,7 +60,7 @@ import java.io.*;
 import java.util.*;
 
 import org.apache.axis.AxisFault ;
-import org.apache.axis.client.http.AdminClient;
+import org.apache.axis.client.AdminClient;
 import org.apache.axis.utils.Debug ;
 import org.apache.axis.utils.Options ;
 import org.apache.axis.utils.QName ;
@@ -81,7 +81,7 @@ public class TestBidBuySample extends TestCase {
     
     public void doTestDeploy () throws Exception {
         String[] args = { "samples/bidbuy/deploy.xml" };
-        new AdminClient().doAdmin(args);
+        AdminClient.main(args);
     }
     
     public void doTest () throws Exception {
