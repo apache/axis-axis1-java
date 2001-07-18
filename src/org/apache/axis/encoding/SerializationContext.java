a7da5ef038f8bc4db4deab3930daee10ceca8fdf
Sam Ruby <rubys@apache.org>
2001-07-18 18:53:25 +0000

    Add encoding back in, this time with UTF-8 spelled correctly.

--- src/org/apache/axis/encoding/SerializationContext.java
+++ src/org/apache/axis/encoding/SerializationContext.java
@@ -270,7 +270,7 @@ public class SerializationContext
         }
 
         if (startOfDocument && sendXMLDecl) {
-            writer.write("<?xml version=\"1.0\"?>");
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
             startOfDocument = false;
         }
         
