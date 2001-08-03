f0c88df0408ebaaf9d067bb95464e1bed0f197f2
Glen Daniels <gdaniels@apache.org>
2001-08-03 21:12:14 +0000

    More examples

---
+++ samples/userguide/example5/BeanService.java
@@ -0,0 +1,25 @@
+
+public class BeanService
+{
+    public String processOrder(Order order)
+    {
+        String sep = System.getProperty("line.separator");
+        
+        String response = "Hi, " + order.getCustomerName() + "!" + sep;
+        
+        response += sep + "You seem to have ordered the following:" + sep;
+        
+        String [] items = order.getItemCodes();
+        int [] quantities = order.getQuantities();
+        
+        for (int i = 0; i < items.length; i++) {
+            response += sep + quantities[i] + " of item : " + items[i];
+        }
+        
+        response += sep + sep +
+                    "If this had been a real order processing system, "+
+                    "we'd probably have charged you about now.";
+        
+        return response;
+    }
+}
---
+++ samples/userguide/example5/Order.java
@@ -0,0 +1,38 @@
+
+/** This is a JavaBean which represents an order for some products.
+ * 
+ * @author Glen Daniels (gdaniels@macromedia.com)
+ */
+public class Order
+{
+    /** Who's ordering  */
+    private String customerName;
+    /** Where do they live  */
+    private String shippingAddress;
+    /** Which items do we want */
+    private String itemCodes[];
+    /** And how many */
+    private int quantities[];
+    
+    // Bean accessors
+    
+    public String getCustomerName()
+    { return customerName; }
+    public void setCustomerName(String name)
+    { customerName = name; }
+    
+    public String getShippingAddress()
+    { return shippingAddress; }
+    public void setShippingAddress(String address)
+    { shippingAddress = address; }
+    
+    public String [] getItemCodes()
+    { return itemCodes; }
+    public void setItemCodes(String [] items)
+    { itemCodes = items; }
+    
+    public int [] getQuantities()
+    { return quantities; }
+    public void setQuantities(int [] quants)
+    { quantities = quants; }
+}
