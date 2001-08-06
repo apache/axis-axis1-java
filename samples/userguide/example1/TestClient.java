3fd1d6ffda58e36c14c1573e51418ce1f8488885
Glen Daniels <gdaniels@apache.org>
2001-08-06 20:21:21 +0000

    Point to Nagoya, don't print stack trace on error.

--- samples/userguide/example1/TestClient.java
+++ samples/userguide/example1/TestClient.java
@@ -4,15 +4,18 @@ import org.apache.axis.client.ServiceClient;
 
 public class TestClient
 {
-   public static void main(String [] args) throws Exception {
-       String endpoint = "http://localhost:8080/axis/servlet/AxisServlet";
-                //"http://nagoya.apache.org:5049/axis/servlet/AxisServlet";
+   public static void main(String [] args) {
+       try {
+           String endpoint = "http://nagoya.apache.org:5049/axis/servlet/AxisServlet";
      
-     ServiceClient client = new ServiceClient(endpoint);
-     String ret = (String)client.invoke("http://soapinterop.org/",
-                                        "echoString",
-                                        new Object [] { "Hello!" });
-  
-     System.out.println("Sent 'Hello!', got '" + ret + "'");
+           ServiceClient client = new ServiceClient(endpoint);
+           String ret = (String)client.invoke("http://soapinterop.org/",
+                                              "echoString",
+                                              new Object [] { "Hello!" });
+           
+           System.out.println("Sent 'Hello!', got '" + ret + "'");
+       } catch (Exception e) {
+           System.err.println(e.toString());
+       }
    }
 }
