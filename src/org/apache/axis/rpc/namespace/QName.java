dfdf7661b8df679cdb88b139969218e75c23a756
Doug Davis <dug@apache.org>
2001-09-26 20:06:53 +0000

    Adding some initial jaxrpc classes/interfaces. Eventually, these will be replaced with the real javax.xml.rpc.* stuff.

---
+++ src/org/apache/axis/rpc/namespace/QName.java
@@ -0,0 +1,8 @@
+// temporary Service interface definition - replace with JAX-RPC
+// when it is ready.
+
+// package javax.xml.rpc ;
+package org.apache.axis.rpc.namespace ;
+
+public class QName {
+}
