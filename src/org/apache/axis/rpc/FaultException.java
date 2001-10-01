4935d71fec86b1bb0f9e4d01a4522055e8b66758
Doug Davis <dug@apache.org>
2001-10-01 12:52:07 +0000

    Add more basic jaxrpc interfaces/classes

---
+++ src/org/apache/axis/rpc/FaultException.java
@@ -0,0 +1,13 @@
+// temporary Service interface definition - replace with JAX-RPC
+// when it is ready.
+
+// package javax.xml.rpc ;
+package org.apache.axis.rpc ;
+
+public class FaultException extends java.lang.Exception {
+   public FaultException() {
+   }
+
+   public FaultException(String str) {
+   }
+}
