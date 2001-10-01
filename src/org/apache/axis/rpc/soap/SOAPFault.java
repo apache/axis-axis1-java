e5bf9543615f845d1a72b9f158a305a697ca7e92
Doug Davis <dug@apache.org>
2001-10-01 12:52:07 +0000

    Add more basic jaxrpc interfaces/classes

---
+++ src/org/apache/axis/rpc/soap/SOAPFault.java
@@ -0,0 +1,9 @@
+// temporary Service interface definition - replace with JAX-RPC
+// when it is ready.
+
+// package javax.xml.rpc ;
+
+package org.apache.axis.rpc.soap ;
+
+public class SOAPFault extends org.apache.axis.rpc.FaultException {
+};
