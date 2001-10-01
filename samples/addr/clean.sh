2b4ef0f8b32e47f5d54489da955d09f28c3fbe3c
Doug Davis <dug@apache.org>
2001-10-01 11:38:24 +0000

    wrong slashes

--- samples/addr/clean.sh
+++ samples/addr/clean.sh
@@ -7,6 +7,6 @@ rm AddressBookSOAPBindingStub.java
 rm AddressHolder.java
 rm Phone.java
 rm PhoneHolder.java
-cd ..\..
-java org.apache.axis.wsdl.Wsdl2java -s samples\addr\AddressBook.wsdl -p samples.addr
-cd samples\addr
+cd ../..
+java org.apache.axis.wsdl.Wsdl2java -s samples/addr/AddressBook.wsdl -p samples.addr
+cd samples/addr
