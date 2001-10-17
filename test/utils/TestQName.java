c59099d166e3fca2f0aeb6547795cc39956b58f6
Berin Loritsch <bloritsch@apache.org>
2001-10-17 19:16:28 +0000

    Add more meaningful messages to tests.

--- test/utils/TestQName.java
+++ test/utils/TestQName.java
@@ -20,16 +20,18 @@ public class TestQName extends TestCase
     public void testQNameDefaultConstructor()
     {
         QName qname = new QName();
-        assertTrue(qname instanceof QName);
-        assertNull(qname.getLocalPart());
+        assertTrue("qname not an instance of QName", qname instanceof QName); // ?? not really necessary.  Compiler won't compile otherwise
+        assertNull("LocalPart was not null", qname.getLocalPart());
     }   
     
     public void testQName2StringConstructor()
     {
         QName qname = new QName("rdf","article");
-        assertNotNull(qname); 
-        assertEquals("rdf", qname.getNamespaceURI()); 
-        assertEquals("article", qname.getLocalPart()); 
+        assertNotNull("qname was null.  Something really wrong here.", qname); 
+        assertEquals("Namespace URI was not 'rdf', it was: " + qname.getNamespaceURI(),
+                     "rdf", qname.getNamespaceURI()); 
+        assertEquals("LocalPart was not 'article', it was: " + qname.getLocalPart(),
+                     "article", qname.getLocalPart()); 
     }
 
     /*
@@ -39,19 +41,22 @@ public class TestQName extends TestCase
         //Element class to test this . . .
         //Element elem = new MockElement();
         QName qname = new QName("PREFIX:LOCALPART", elem);
-        assertNotNull(qname); 
-        assertEquals("PREFIX", qname.getNamespaceURI()); 
-        assertEquals("LOCALPART", qname.getNamespaceURI()); 
+        assertNotNull("qname was null", qname); 
+        assertEquals("Namespace URI was not 'PREFIX', it was: " + qname.getNamespaceURI(),
+                     "PREFIX", qname.getNamespaceURI()); 
+        assertEquals("LocalPart was not 'LOCALPART', it was: " + qname.getLocalPart(),
+                     "LOCALPART", qname.getLocalPart()); 
     }
      */
 
     public void testToString()
     {
         QName qname = new QName("PREFIX", "LOCALPART");
-        assertEquals("PREFIX:LOCALPART", qname.toString());
+        assertEquals("qname was not the same as 'PREFIX:LOCALPART', it was: " + qname.toString(),
+                     "PREFIX:LOCALPART", qname.toString());
 
         qname.setNamespaceURI(null);
-        assertEquals("LOCALPART", qname.toString());
+        assertEquals("qname was not the same as 'LOCALPART', is was: " + qname.toString(), "LOCALPART", qname.toString());
     }
 
     public void testEquals()
@@ -65,15 +70,15 @@ public class TestQName extends TestCase
         ////QName qname5 = new QName("PREFIX:LOCALPART", elem);
 
         // the following should NOT throw a NullPointerException
-        assertTrue(!qname1.equals(qname2));
+        assertTrue("qname1 is the same as qname2", !qname1.equals(qname2));
        
         //Note: this test is comparing the same two QName objects as above, but
         //due to the order and the implementation of the QName.equals() method,
         //this test passes without incurring a NullPointerException. 
-        assertTrue(!qname2.equals(qname1));
+        assertTrue("qname2 is the same as qname1", !qname2.equals(qname1));
 
-        assertTrue(qname2.equals(qname3));
-        assertTrue(!qname3.equals(qname4));
+        assertTrue("qname2 is not the same as qname2", qname2.equals(qname3));
+        assertTrue("qname3 is the same as qname4", !qname3.equals(qname4));
     }
     
     public void testHashCode()
@@ -81,7 +86,7 @@ public class TestQName extends TestCase
         QName control = new QName("xsl", "text");
         QName compare = new QName("xsl", "text");
         QName contrast = new QName("xso", "text");
-        assertEquals(control.hashCode(), compare.hashCode());
-        assertTrue(!(control.hashCode() == contrast.hashCode()));
+        assertEquals("control hashcode does not equal compare.hashcode", control.hashCode(), compare.hashCode());
+        assertTrue("control hashcode is not equivalent to compare.hashcode", !(control.hashCode() == contrast.hashCode()));
     }
 }
