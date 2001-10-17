23e94146761fffd977de77eabaf7b971bf818c85
Berin Loritsch <bloritsch@apache.org>
2001-10-17 13:36:03 +0000

    Add support to copy precreated implementations to the the work area--so that we have something to test.

---
+++ samples/addr/AddressBookSOAPBindingImpl.java
@@ -0,0 +1,21 @@
+/**
+ * AddressBookSOAPBindingImpl.java
+ *
+ * This file was hand modified from the Emmitter generated code.
+ */
+
+package samples.addr;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class AddressBookSOAPBindingImpl implements AddressBook {
+    Map addresses = new HashMap();
+
+    public void addEntry(java.lang.String name, Address address) throws java.rmi.RemoteException {
+        this.addresses.put(name, address);
+    }
+    public Address getAddressFromName(java.lang.String name) throws java.rmi.RemoteException {
+        return (Address) this.addresses.get(name);
+    }
+}
