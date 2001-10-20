d5fcdb0bd9e0a6464dc1362281e8860960847d68
Rajesh Kumar <rkumar@apache.org>
2001-10-20 08:49:49 +0000

    Test cases need to be aware of multiple namespaces. Will address this on surfacing this through Wsdl2Java In the meanwhile, use the documents targetnamespace for types as well.

--- samples/addr/AddressBookSOAPBindingImpl.java
+++ samples/addr/AddressBookSOAPBindingImpl.java
@@ -12,10 +12,10 @@ import java.util.Map;
 public class AddressBookSOAPBindingImpl implements AddressBook {
     private Map addresses = new HashMap();
 
-    public void addEntry(java.lang.String name, samples.addr.xml_soap_address_demo.Address address) throws java.rmi.RemoteException {
+    public void addEntry(java.lang.String name, samples.addr.Address address) throws java.rmi.RemoteException {
         this.addresses.put(name, address);
     }
-    public samples.addr.xml_soap_address_demo.Address getAddressFromName(java.lang.String name) throws java.rmi.RemoteException {
-        return (samples.addr.xml_soap_address_demo.Address) this.addresses.get(name);
+    public samples.addr.Address getAddressFromName(java.lang.String name) throws java.rmi.RemoteException {
+        return (samples.addr.Address) this.addresses.get(name);
     }
 }
