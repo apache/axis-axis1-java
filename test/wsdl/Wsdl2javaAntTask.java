e76c32c33a11448560e5952e90ab138607d9972d
Davanum Srinivas <dims@apache.org>
2001-10-24 18:20:39 +0000

    Move src/org/apache/axis/wsdl/test to test/wsdl

--- test/wsdl/Wsdl2javaAntTask.java
+++ test/wsdl/Wsdl2javaAntTask.java
@@ -51,7 +51,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
-package org.apache.axis.wsdl.test;
+package test.wsdl;
 
 import org.apache.axis.wsdl.Emitter; 
 import org.apache.tools.ant.BuildException;
--- test/wsdl/Wsdl2javaTestSuite.java
+++ test/wsdl/Wsdl2javaTestSuite.java
@@ -52,7 +52,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
-package org.apache.axis.wsdl.test;
+package test.wsdl;
 
 import junit.framework.AssertionFailedError;
 import junit.framework.TestResult;
