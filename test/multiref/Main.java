41b9a3ffbf6473a7eadf5a6899e27efa3c8fd8a7
Richard J. Scheuerle, Jr <scheu@apache.org>
2001-11-06 21:41:55 +0000

    Installed new wsdl functional test: MultiRefTest This test verifies correct multi-reference serialization/deserialization. PR: Obtained from: Submitted by: Reviewed by:

---
+++ test/multiref/Main.java
@@ -0,0 +1,264 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2000, International
+ * Business Machines, Inc., http://www.apache.org.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package test.multiref;
+
+import org.apache.axis.utils.Options;
+
+import java.net.URL;
+
+
+
+/**
+ * This class shows/tests how multi-referenced objects are preserved.
+ *
+ * @author Rich Scheuerle (scheu@us.ibm.com)     
+ */
+public class Main {
+
+    public static void main (String[] args) throws Exception {
+        Options opts = new Options(args);
+        
+        MultiRefTestService service = new MultiRefTestService();
+        opts.setDefaultURL( service.getMultiRefTestAddress() );
+        URL serviceURL = new URL(opts.getURL());
+        if (serviceURL == null)
+            serviceURL = new URL(service.getMultiRefTestAddress());
+
+        MultiRefTest remoteTest = service.getMultiRefTest(serviceURL);
+
+        // ----------------------
+        // Create a simple tree
+        Node t = new Node(new Node(null, null, 1), new Node(null, null, 2), 0);
+        NodeHolder holder = new NodeHolder(t);
+        
+        // Test for simple tree
+        int rc = remoteTest.testSimpleTree(holder);
+        if (rc == 0) {
+            System.err.println("Passed testSimpleTree 1");
+        } else {
+            System.err.println("Failed testSimpleTree 1");
+            throw new Exception("Failed testSimpleTree 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testSimpleTree(holder);
+        if (rc == 0) {
+            System.err.println("Passed testSimpleTree 2");
+        } else {
+            System.err.println("Failed testSimpleTree 2");
+            throw new Exception("Failed testSimpleTree 2 with "+rc);
+        }
+
+        // ----------------------
+        // Create a diamond
+        t = new Node(new Node(null, null, 1), new Node(null, null, 2), 0);
+        Node d = new Node(null, null, 3);
+        t.getLeft().setRight(d);
+        t.getRight().setLeft(d);
+        holder = new NodeHolder(t);
+
+        // Test for a diamond
+        rc = remoteTest.testDiamond(holder);
+        if (rc == 0) {
+            System.err.println("Passed testDiamond 1");
+        } else {
+            System.err.println("Failed testDiamond 1");
+            throw new Exception("Failed testDiamond 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testDiamond(holder);
+        if (rc == 0) {
+            System.err.println("Passed testDiamond 2");
+        } else {
+            System.err.println("Failed testDiamond 2");
+            throw new Exception("Failed testDiamond 2 with "+rc);
+        }
+
+        // ----------------------
+        // Create a 'loop' tree.  The children of the root have children that reference the root.
+        t = new Node(null, null, 0);
+        t.setLeft(new Node(t,t,1));
+        t.setRight(new Node(t,t,2));
+        holder = new NodeHolder(t);
+
+        // Test for loops
+        rc = remoteTest.testLoop(holder);
+        if (rc == 0) {
+            System.err.println("Passed testLoop 1");
+        } else {
+            System.err.println("Failed testLoop 1");
+            throw new Exception("Failed testLoop 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testLoop(holder);
+        if (rc == 0) {
+            System.err.println("Passed testLoop 2");
+        } else {
+            System.err.println("Failed testLoop 2");
+            throw new Exception("Failed testLoop 2 with "+rc);
+        }
+
+        // ----------------------
+        // Test passing of the same node argument.
+        t = new Node(null, null, 0);
+        NodeHolder holder1 = new NodeHolder(t);
+        NodeHolder holder2 = new NodeHolder(t);
+
+        // Test
+        rc = remoteTest.testSameArgs(holder1, holder2);
+        if (rc == 0) {
+            System.err.println("Passed testSameArgs 1");
+        } else {
+            System.err.println("Failed testSameArgs 1");
+            throw new Exception("Failed testSameArgs 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testSameArgs(holder1,holder2);
+        if (rc == 0) {
+            System.err.println("Passed testSameArgs 2");
+        } else {
+            System.err.println("Failed testSameArgs 2");
+            throw new Exception("Failed testSameArgs 2 with "+rc);
+        } 
+
+        // ----------------------
+        // Test args referencing same node.
+        Node t1 = new Node(null, null, 0);
+        Node t2 = new Node(null, null, 1);
+        Node s  = new Node(null, null, 2);
+        t1.setRight(s);
+        t2.setLeft(s);
+        holder1 = new NodeHolder(t1);
+        holder2 = new NodeHolder(t2);
+
+        // Test
+        rc = remoteTest.testArgsRefSameNode(holder1, holder2);
+        if (rc == 0) {
+            System.err.println("Passed testArgsRefSameNode 1");
+        } else {
+            System.err.println("Failed testArgsRefSameNode 1");
+            throw new Exception("Failed testArgsRefSameNode 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testArgsRefSameNode(holder1,holder2);
+        if (rc == 0) {
+            System.err.println("Passed testArgsRefSameNode 2");
+        } else {
+            System.err.println("Failed testArgsRefSameNode 2");
+            throw new Exception("Failed testArgsRefSameNode 2 with "+rc);
+        }
+        // ----------------------
+        // Test args referencing each other.
+        t1 = new Node(null, null, 0);
+        t2 = new Node(t1, t1, 1);
+        t1.setRight(t2);
+        t1.setLeft(t2);
+        holder1 = new NodeHolder(t1);
+        holder2 = new NodeHolder(t2);
+
+        // Test
+        rc = remoteTest.testArgsRefEachOther(holder1, holder2);
+        if (rc == 0) {
+            System.err.println("Passed testArgsRefEachOther 1");
+        } else {
+            System.err.println("Failed testArgsRefEachOther 1");
+            throw new Exception("Failed testArgsRefEachOther 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testArgsRefEachOther(holder1,holder2);
+        if (rc == 0) {
+            System.err.println("Passed testArgsRefEachOther 2");
+        } else {
+            System.err.println("Failed testArgsRefEachOther 2");
+            throw new Exception("Failed testArgsRefEachOther 2 with "+rc);
+        }
+
+        // ----------------------
+        // Create self referencing node
+        t = new Node(null, null, 0);
+        t.setLeft(t);
+        t.setRight(t);
+        holder = new NodeHolder(t);
+
+        // Self-Ref Test
+        rc = remoteTest.testSelfRef(holder);
+        if (rc == 0) {
+            System.err.println("Passed testSelfRef 1");
+        } else {
+            System.err.println("Failed testSelfRef 1");
+            throw new Exception("Failed testSelfRef 1 with "+rc);
+        }
+        // Test returns the tree.  To make sure it returned it successfully,
+        // invoke the test again!
+        rc = remoteTest.testSelfRef(holder);
+        if (rc == 0) {
+            System.err.println("Passed testSelfRef 2");
+        } else {
+            System.err.println("Failed testSelfRef 2");
+            throw new Exception("Failed testSelfRef 2 with "+rc);
+        }
+ 
+        return;
+    }
+}
+
---
+++ test/multiref/MultiRefTestSOAPBindingImpl.java
@@ -0,0 +1,210 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2000, International
+ * Business Machines, Inc., http://www.apache.org.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package test.multiref;
+
+/**
+ * MultiRefTestSOAPBindingImpl.java
+ *
+ * @author Rich Scheuerle (scheu@us.ibm.com)
+ */
+public class MultiRefTestSOAPBindingImpl implements test.multiref.MultiRefTest {
+
+    /**
+     * Tests for the following arrangement of nodes:
+     *        0
+     *       / \
+     *      1   2
+     */
+    public int testSimpleTree(test.multiref.NodeHolder root) throws java.rmi.RemoteException {
+        Node t = root._value;    // Root of tree 
+        Node l = t.getLeft();    // Left side
+        Node r = t.getRight();   // Right side
+
+        if (t != null && l != null && r != null &&
+            r != l && 
+            l.getLeft() == null &&
+            l.getRight()== null &&
+            r.getLeft() == null &&
+            r.getRight()== null)
+            return 0;  // Great
+        
+        return 1;  // Bad       
+    }
+
+    /**
+     * Tests for the following arrangement of nodes:
+     *        0
+     *       / \
+     *      1   2
+     *       \ /
+     *        3
+     */
+    public int testDiamond(test.multiref.NodeHolder root) throws java.rmi.RemoteException {
+        Node t = root._value;    // Root of tree 
+        Node l = t.getLeft();    // Left side
+        Node r = t.getRight();   // Right side
+
+        if (t != null && l != null && r != null &&
+            r != l && 
+            l.getLeft() == null &&
+            r.getRight()== null &&
+            l.getRight()!= null &&
+            l.getRight()== r.getLeft())
+            return 0;  // Great
+        
+        return 1;  // Bad
+    }
+
+    /**
+     * Tests for the following arrangement of nodes:
+     *        0
+     *       / \
+     *      1   2   and the children of 1 & 2 are backward references to 0
+     */
+    public int testLoop(test.multiref.NodeHolder root) throws java.rmi.RemoteException {
+        Node t = root._value;    // Root of tree 
+        Node l = t.getLeft();    // Left side
+        Node r = t.getRight();   // Right side
+
+        if (t != null && l != null && r != null &&
+            r != l && 
+            l.getLeft() == t &&
+            l.getRight()== t &&
+            r.getLeft() == t &&
+            r.getRight()== t)
+            return 0;  // Great
+        
+        return 1;  // Bad       
+    }
+    /**
+     * Tests for the following arrangement of nodes:
+     *        0
+     *        and the children of 0 are backward references to 0
+     */
+    public int testSelfRef(test.multiref.NodeHolder root) throws java.rmi.RemoteException {
+        Node t = root._value;    // Root of tree 
+        Node l = t.getLeft();    // Left side
+        Node r = t.getRight();   // Right side
+
+        if (t != null && l != null && r != null &&
+            t == l && t == r) 
+            return 0;  // Great
+        
+        return 1;  // Bad       
+    }
+
+    /**
+     * Tests that both arguments are the same node & the nodes don't have children
+     */
+    public int testSameArgs(test.multiref.NodeHolder root1,test.multiref.NodeHolder root2)
+        throws java.rmi.RemoteException {
+        Node t1 = root1._value;    // Root1 of tree 
+        Node t2 = root2._value;    // Root2 of tree 
+
+        if (t1 != null && t2 != null  &&
+            t1 == t2 &&
+            t1.getRight() == null &&
+            t1.getLeft()  == null)
+            return 0;  // Great
+        
+        return 1;  // Bad       
+    }
+
+    /**
+     * Tests for the following arrangement of nodes:
+     *      0   1
+     *       \ /
+     *        2     where 0 and 1 are the argument nodes.                      
+     */
+    public int testArgsRefSameNode(test.multiref.NodeHolder root1,test.multiref.NodeHolder root2)
+        throws java.rmi.RemoteException {
+        Node t1 = root1._value;    // Root1 of tree 
+        Node t2 = root2._value;    // Root2 of tree 
+
+        if (t1 != null && t2 != null &&
+            t1 != t2 &&
+            t1.getLeft()  == null &&
+            t2.getRight() == null &&
+            t1.getRight() != null &&
+            t1.getRight() == t2.getLeft() &&
+            t1.getRight().getRight() == null &&
+            t1.getRight().getLeft() == null)
+            return 0;  // Great
+        
+        return 1;  // Bad       
+    }
+    /**
+     * Tests for two node arguments that reference each other.
+     */
+    public int testArgsRefEachOther(test.multiref.NodeHolder root1,test.multiref.NodeHolder root2)
+        throws java.rmi.RemoteException {
+        Node t1 = root1._value;    // Root1 of tree 
+        Node t2 = root2._value;    // Root2 of tree 
+
+        if (t1 != null && t2 != null &&
+            t1 != t2 &&
+            t1.getLeft()  == t2 &&
+            t1.getRight() == t2 &&
+            t2.getLeft() == t1 &&
+            t2.getRight() == t1)
+            return 0;  // Great
+        
+        return 1;  // Bad       
+    }
+
+}
