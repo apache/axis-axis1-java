2d8091df69d99241b20b9f37eed9413d05b1a176
Glen Daniels <gdaniels@apache.org>
2001-11-07 22:23:08 +0000

    Add a test to make sure that setting the returnType in a Call allows us to deserialize untyped parameters.
    ** NOTE : THIS IS CURRENTLY BROKEN **

--- test/encoding/TestXsiType.java
+++ test/encoding/TestXsiType.java
@@ -2,8 +2,13 @@ package test.encoding;
 
 import junit.framework.TestCase;
 import org.apache.axis.MessageContext;
+import org.apache.axis.AxisEngine;
+import org.apache.axis.transport.local.LocalTransport;
+import org.apache.axis.providers.java.RPCProvider;
+import org.apache.axis.handlers.soap.SOAPService;
 import org.apache.axis.client.Call;
 import org.apache.axis.encoding.SerializationContext;
+import org.apache.axis.encoding.XMLType;
 import org.apache.axis.message.RPCElement;
 import org.apache.axis.message.RPCParam;
 import org.apache.axis.message.SOAPEnvelope;
@@ -21,11 +26,19 @@ public class TestXsiType extends TestCase {
     private String header;
     private String footer;
     private AxisServer server = new AxisServer();
+    
+    public TestXsiType()
+    {
+        super("testing");
+    }
 
     public TestXsiType(String name) {
         super(name);
     }
 
+    /**
+     * Trivial test just to make sure there aren't xsi:type attributes
+     */ 
     public void testNoXsiTypes()
        throws Exception
     {
@@ -53,11 +66,53 @@ public class TestXsiType extends TestCase {
         assertTrue("Found unexpected xsi:type!",
                    msgString.indexOf("xsi:type") == -1);
     }
-
+    
+    /**
+     * More complex test which checks to confirm that we can still
+     * in fact deserialize if we know the type via the Call object.
+     */ 
+    public void testTypelessDeserialization() throws Exception
+    {
+        // Set up a server to NOT send XSI types, and deploy
+        // this class as a service there.
+        
+        AxisServer server = new AxisServer();
+        server.addOption(AxisEngine.PROP_SEND_XSI, Boolean.FALSE);
+        
+        SOAPService service = new SOAPService(new RPCProvider());
+        service.addOption("className", "test.encoding.TestXsiType");
+        service.addOption("methodName", "*");
+        server.deployService("TestService", service);
+        
+        // Call that same server, accessing a method we know returns
+        // a double.  We should figure this out and deserialize it
+        // correctly, even without the xsi:type attribute, because
+        // we set the return type manually.
+        
+        Call call = new Call();
+        call.setTransport(new LocalTransport(server));
+        call.setReturnType(XMLType.XSD_DOUBLE);
+        
+        Object result = call.invoke("TestService", 
+                                    "serviceMethod", 
+                                    new Object [] {});
+        
+        assertTrue("Return value was not the expected type (Double)!",
+                    (result instanceof Double));
+    }
+    
+    /**
+     * A method for our service, returning a double.
+     */ 
+    public double serviceMethod()
+    {
+        return 3.14159;
+    }
 
     public static void main(String [] args) throws Exception
     {
         TestXsiType tester = new TestXsiType("test");
         tester.testNoXsiTypes();
+        tester.testTypelessDeserialization();
     }
 }
