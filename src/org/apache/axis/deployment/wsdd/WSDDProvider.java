03ca1989ce394fabb317b768fdb1790c7030e4d1
Sam Ruby <rubys@apache.org>
2001-12-12 18:57:11 +0000

    Submitted by:   Igor  Sedukhin <Igor.Sedukhin@ca.com>
    As post alpha2 deployment architecture changed, I was trying to register a
    custom provider (as an alternative to registering a generic handler).
    WSDDProvider API allows me to, but there is inconsistency with the rest of
    the code: everywhere the assumption is that WSDDProvider.providers
    hashtable contains instances of WSDDProvider, the registerProvider API takes
    class, and obviously, there is no way to cast an object to a class.
    WSDDProvider.providers hashtable is also declared private, so there is no
    way around -- the API has to be fixed. The already existing static code
    block in the beginning of the WSDDProvider.java validates my suggestion.

--- src/org/apache/axis/deployment/wsdd/WSDDProvider.java
+++ src/org/apache/axis/deployment/wsdd/WSDDProvider.java
@@ -102,9 +102,9 @@ public abstract class WSDDProvider
      * @param uri XXX
      * @param _class XXX
      */
-    public static void registerProvider(String uri, Class _class)
+    public static void registerProvider(QName uri, WSDDProvider prov)
     {
-        providers.put(uri, _class);
+        providers.put(uri, prov);
     }
 
     /**
