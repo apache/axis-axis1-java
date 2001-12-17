8f07911e0961173a2f58bfb79eaf06e61b311886
Doug Davis <dug@apache.org>
2001-12-17 00:57:41 +0000

    Java has a bit of an "issue" in that you can ask for (Element[]).class but you can't ask a ClassLoader to actually load that class - so you can't dynamically invoke a method with Element[] as one of its parameters when the array of Class's is supposed to have been loaded from a particular classloader (or at least I couldn't figure out how to do it). So switched everything from Element[] to Vector, and added a test for it.

---
+++ samples/message/MessageService.java
@@ -0,0 +1,16 @@
+package samples.message ;
+
+import org.w3c.dom.Element ;
+import org.apache.axis.MessageContext ;
+import java.util.Vector ;
+
+public class MessageService {
+    public Element[] echoElements(MessageContext msgC, Vector elems) {
+        Element[]  result = new Element[elems.size()];
+
+        for ( int i = 0 ; i < elems.size() ; i++ )
+            result[i] = (Element) elems.get(i);
+        
+        return( result );
+    }
+}
