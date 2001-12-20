66f8f29b73303e7e4f799dc9c322ed0837c56cde
Russell Butek <butek@apache.org>
2001-12-20 14:56:47 +0000

    Fixed a bug found by Andy Carlson (andyslistbox@hotmail.com).  When we put in the fixes to handle operation names that were Java keywords, we didn't go far enough.  We made sure that, when a operation name like "new" became the Java method "_new" we could still map back to "new".  But there are other manglings that we do for Java names (like changing "Capitalized" to "capitalized").  The method xmlNameToJava must be called in RPCProvider.
    I made this fix and I added this case to the JavaNames.wsdl test.

--- src/org/apache/axis/utils/JavaUtils.java
+++ src/org/apache/axis/utils/JavaUtils.java
@@ -229,6 +229,53 @@ public class JavaUtils
         return  keywordPrefix + keyword;
      }
 
+    /**
+     * Map an XML name to a valid Java identifier
+     */
+    public static String xmlNameToJava(String name)
+    {
+        char[] nameArray = name.toCharArray();
+        int len = name.length();
+        StringBuffer result = new StringBuffer(len);
+        
+        // First character, lower case
+        int i = 0;
+        while (i < name.length() 
+                && !Character.isLetter(nameArray[i])) {
+            i++;
+        }
+        if (i < name.length()) {
+            result.append(Character.toLowerCase(nameArray[i]));
+        }
+        else {
+            result.append("_" + nameArray[0]);
+        }
+        
+        // The rest of the string
+        boolean wordStart = false;
+        for(int j = i + 1; j < len; ++j) {
+            char c = nameArray[j];
+
+            // if this is a bad char, skip it a remember to capitolize next
+            // good character we encounter
+            if( !Character.isLetterOrDigit(c)) {
+                wordStart = true;
+                continue;
+            }
+            result.append( wordStart ? Character.toUpperCase(c) : c );
+            wordStart = false;
+        }
+        
+        // covert back to a String
+        String newName = result.toString();
+        
+        // check for Java keywords
+        if (isJavaKeyword(newName))
+            newName = makeNonJavaKeyword(newName);
+        
+        return newName;
+    } // xmlNameToJava
+
     // Message resource bundle.
     private static ResourceBundle messages = null;
 
