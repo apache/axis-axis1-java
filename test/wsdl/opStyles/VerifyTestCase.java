125b6a04fe8daeb98050c0555262070b33538c60
Russell Butek <butek@apache.org>
2002-01-16 14:45:53 +0000

    This makes the operation style test a bit better by verifying that the expected methods are really there and the ones that shouldn't be aren't (solicitResopnse and notification style methods).

---
+++ test/wsdl/opStyles/VerifyTestCase.java
@@ -0,0 +1,54 @@
+package test.wsdl.opStyles;
+
+import java.lang.reflect.Method;
+
+import java.util.Vector;
+
+// This test makes sure that the OpStyles interface ONLY has oneway and
+// requestResponse methods and that no other methods, particularly
+// solicitResponse and notification, exist.
+
+public class VerifyTestCase extends junit.framework.TestCase {
+    public VerifyTestCase(String name) {
+        super(name);
+    }
+
+    public void testOpStyles() {
+        Method[] methods =
+                test.wsdl.opStyles.OpStyles.class.getDeclaredMethods();
+        boolean sawOneway = false;
+        boolean sawRequestResponse = false;
+        boolean sawSolicitResponse = false;
+        boolean sawNotification = false;
+        Vector  others = new Vector();
+        for (int i = 0; i < methods.length; ++i) {
+            String name = methods[i].getName();
+            if ("oneway".equals(name)) {
+                sawOneway = true;
+            }
+            else if ("requestResponse".equals(name)) {
+                sawRequestResponse = true;
+            }
+            else {
+                others.add(name);
+            }
+        }
+        assertTrue("Expected method oneWay does not exist on OpStyles", sawOneway == true);
+        assertTrue("Expected method requestResponse does not exist on OpStyles",
+                sawRequestResponse == true);
+        if (others.size() > 0) {
+            String message = "Methods exist on OpStyles but should not:  ";
+            boolean needComma = false;
+            for (int i = 0; i < others.size(); ++i) {
+                if (needComma) {
+                    message += ", ";
+                }
+                else {
+                    needComma = true;
+                }
+                message += (String) others.get(i);
+            }
+        }
+    } // testOpStyles
+} // class VerifyTestCase
+
