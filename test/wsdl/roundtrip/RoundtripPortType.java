a624c3f197db9cd5d8e384f08f4add975a45a330
Russell Butek <butek@apache.org>
2002-02-12 22:10:19 +0000

    Roundtrip test case for various types. Contributed by Brent Ulbricht (ulbricht@us.ibm.com).  From his note:
    I have attached a test primarily to verify a roundtrip between Java2WSDL
    and WSDL2Java for primitives, standard Java classes, nillables, arrays,
    multiple arrays, and JAX-RPC value types.

---
+++ test/wsdl/roundtrip/RoundtripPortType.java
@@ -0,0 +1,160 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Axis" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package test.wsdl.roundtrip;
+
+import test.wsdl.roundtrip.Investment;
+import test.wsdl.roundtrip.BondInvestment;
+import test.wsdl.roundtrip.StockInvestment;
+import test.wsdl.roundtrip.PreferredStockInvestment;
+import test.wsdl.roundtrip.CallOptions;
+
+/**
+ * The RoundtripPortType interface defines the methods necessary when
+ * when implementing this interface.  
+ *
+ * @version   1.00  06 Feb 2002
+ * @author    Brent Ulbricht
+ */
+public interface RoundtripPortType {
+
+    public boolean methodBoolean(boolean inBoolean)
+        throws java.rmi.RemoteException;
+    public byte methodByte(byte inByte)
+        throws java.rmi.RemoteException;
+    public short methodShort(short inShort)
+        throws java.rmi.RemoteException;
+    public int methodInt(int inInt)
+        throws java.rmi.RemoteException;
+    public long methodLong(long inLong)
+        throws java.rmi.RemoteException;
+    public float methodFloat(float inFloat)
+        throws java.rmi.RemoteException;
+    public double methodDouble(double inDouble)
+        throws java.rmi.RemoteException;
+    public java.lang.String methodString(java.lang.String inString)
+        throws java.rmi.RemoteException;
+    public java.math.BigInteger methodBigInteger(java.math.BigInteger inInteger)
+        throws java.rmi.RemoteException;
+    public java.math.BigDecimal methodBigDecimal(java.math.BigDecimal inDecimal)
+        throws java.rmi.RemoteException;
+    public java.util.Date methodDateTime(java.util.Date inDateTime)
+        throws java.rmi.RemoteException;
+    public byte[] methodByteArray(byte[] inByteArray)
+        throws java.rmi.RemoteException;
+    public Byte[] methodSoapByteArray(Byte[] inSoapByteArray)
+        throws java.rmi.RemoteException;
+    public void methodAllTypesIn(java.lang.String string,
+                                 java.math.BigInteger integer,
+                                 java.math.BigDecimal decimal,
+                                 java.util.Date dateTime,
+                                 boolean _boolean,
+                                 byte _byte,
+                                 short _short,
+                                 int _int,
+                                 long _long,
+                                 float _float,
+                                 double _double,
+                                 byte[] base64Binary,
+                                 java.lang.Boolean soapBoolean,
+                                 java.lang.Byte soapByte,
+                                 java.lang.Short soapShort,
+                                 java.lang.Integer soapInt,
+                                 java.lang.Long soapLong,
+                                 java.lang.Float soapFloat,
+                                 java.lang.Double soapDouble,
+                                 java.lang.Byte[] soapBase64)
+        throws java.rmi.RemoteException;
+    public int[] methodIntArrayInOut(int[] inIntArray)
+        throws java.rmi.RemoteException;
+    public void methodIntArrayIn(int[] inIntArray)
+        throws java.rmi.RemoteException;
+    public int[] methodIntArrayOut()
+        throws java.rmi.RemoteException;
+    public String[][] methodStringMArrayInOut(String[][] inStringArray)
+        throws java.rmi.RemoteException;
+    public void methodStringMArrayIn(String[][] inStringArray)
+        throws java.rmi.RemoteException;
+    public String[][] methodStringMArrayOut()
+        throws java.rmi.RemoteException;
+    public void methodBondInvestmentIn(BondInvestment bondInvestment)
+        throws java.rmi.RemoteException;
+    public BondInvestment methodBondInvestmentOut()
+        throws java.rmi.RemoteException;
+    public BondInvestment methodBondInvestmentInOut(BondInvestment bondInvestment)
+        throws java.rmi.RemoteException;
+    public float getRealtimeLastTradePrice(StockInvestment stockInvestment)
+        throws java.rmi.RemoteException;
+    public PreferredStockInvestment getDividends(PreferredStockInvestment preferredStock)
+        throws java.rmi.RemoteException;
+    public CallOptions[] methodCallOptions(CallOptions[] callOptions)
+        throws java.rmi.RemoteException;
+    public java.lang.Float methodSoapFloat(java.lang.Float inSoapFloat)
+        throws java.rmi.RemoteException;
+    public java.lang.Double methodSoapDouble(java.lang.Double inSoapDouble)
+        throws java.rmi.RemoteException;
+    public java.lang.Boolean methodSoapBoolean(java.lang.Boolean inSoapBoolean)
+        throws java.rmi.RemoteException;
+    public java.lang.Byte methodSoapByte(java.lang.Byte inSoapByte)
+        throws java.rmi.RemoteException;
+    public java.lang.Short methodSoapShort(java.lang.Short inSoapShort)
+        throws java.rmi.RemoteException;
+    public java.lang.Integer methodSoapInt(java.lang.Integer inSoapInt)
+        throws java.rmi.RemoteException;
+    public java.lang.Long methodSoapLong(java.lang.Long inSoapLong)
+        throws java.rmi.RemoteException;
+
+} // RoundtripPortType
+
