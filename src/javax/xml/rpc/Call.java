5baec34d415a2f61273bcace247b63c26b2b976c
glyn <dev-null@apache.org>
2002-02-22 13:55:39 +0000

    Superficial comment changes.

--- src/javax/xml/rpc/Call.java
+++ src/javax/xml/rpc/Call.java
@@ -63,51 +63,56 @@ import javax.xml.rpc.namespace.QName;
  * invocation of a service port. The javax.xml.rpc.Service interface
  * acts as a factory for the creation of Call instances.
  * 
- *   Once a Call instance is created, various setter and getter 
+ * Once a Call instance is created, various setter and getter 
  * methods may be used to configure this Call instance. The 
  * properties configured on a Call instance include the 
  * following:
- * <UL>
- * <LI>Name of a specific operation and port type for the 
+ * <ul>
+ * <li>Name of a specific operation and port type for the 
  *   target service port
- * <LI>Encoding style specified as a namespace URI
- * <LI>Endpoint address of the target service port
- * <LI>Properties specific to the binding to an XML based 
+ * <li>Encoding style specified as a namespace URI
+ * <li>Endpoint address of the target service port
+ * <li>Properties specific to the binding to an XML based 
  * protocol and transpor
- * <LI>Name, type and mode (IN, INOUT, OUT) of the parameters
- * <LI>Return type
- * </UL>
+ * <li>Name, type and mode (IN, INOUT, OUT) of the parameters
+ * <li>Return type
+ * </ul>
  *
  * @version 0.1
  */
 public interface Call {
 
     /**
-     * Is the caller required to provide the parameter and return type specification?  If true, then
-     * addParameter and setReturnType MUST be called to provide the meta data.  If false, then
-     * addParameter and setReturnType CANNOT be called because the Call object already has the meta
-     * data and the user is not allowed to mess with it.  These methods throw JAXRPCException if
-     * this method returns false.
+     * Is the caller required to provide the parameter and return type
+     * specification?  If true, then addParameter and setReturnType MUST be
+     * called to provide the meta data. If false, then addParameter and
+     * setReturnType CANNOT be called because the Call object already has the
+     * metadata and the user is not allowed to mess with it. These methods
+     * throw JAXRPCException if this method returns false.
      */
     public boolean isParameterAndReturnSpecRequired();
 
     /**
-     * Adds a parameter type and mode for a specific operation. Note that the client code is not required to call any 
-     * addParameter and setReturnType methods before calling the invoke method. A Call implementation class can 
-     * determine the parameter types by using the Java reflection and configured type mapping registry.
+     * Adds a parameter type and mode for a specific operation. Note that the
+     * client code is not required to call any addParameter and setReturnType
+     * methods before calling the invoke method. A Call implementation class
+     * can determine the parameter types by using the Java reflection and
+     * configured type mapping registry.
      *
      * @param paramName - Name of the parameter
      * @param paramType - XML datatype of the parameter
-     * @param parameterMode - Mode of the parameter-whether PARAM_MODE_IN, PARAM_MODE_OUT or PARAM_MODE_INOUT
-     * @exception JAXRPCException - if isParameterAndReturnSpecRequired returns false, then
-     * addParameter will throw JAXRPCException.
+     * @param parameterMode - Mode of the parameter-whether PARAM_MODE_IN,
+     *                        PARAM_MODE_OUT or PARAM_MODE_INOUT
+     * @exception JAXRPCException - if isParameterAndReturnSpecRequired returns
+     *                              false, then addParameter will throw
+     *                              JAXRPCException.
      */
     public void addParameter(String paramName, QName paramType,
             ParameterMode parameterMode);
 
     /**
-     * Given a parameter name, return the QName of its type.  If the parameter doesn't exist, this
-     * method returns null.
+     * Given a parameter name, return the QName of its type. If the parameter
+     * doesn't exist, this method returns null.
      *
      * @param paramName - Name of the parameter.
      */
@@ -117,8 +122,8 @@ public interface Call {
      * Sets the return type for a specific operation.
      *
      * @param xmlType - QName of the data type of the return value
-     * @exception JAXRPCException - if isParameterAndReturnSpecRequired returns false, then
-     * setReturnType will throw JAXRPCException.
+     * @exception JAXRPCException - if isParameterAndReturnSpecRequired returns
+     * false, then setReturnType will throw JAXRPCException.
      */
     public void setReturnType(QName xmlType);
 
@@ -129,8 +134,8 @@ public interface Call {
 
     /**
      * Removes all specified parameters from this Call instance.
-     * @exception JAXRPCException - if isParameterAndReturnSpecRequired returns false, then
-     * removeAllParameters will throw JAXRPCException.
+     * @exception JAXRPCException - if isParameterAndReturnSpecRequired returns
+     * false, then removeAllParameters will throw JAXRPCException.
      */
     public void removeAllParameters();
 
@@ -144,7 +149,8 @@ public interface Call {
     /**
      * Sets the name of the operation to be invoked using this Call instance.
      *
-     * @param operationName - QName of the operation to be invoked using the Call instance
+     * @param operationName - QName of the operation to be invoked using the
+     *                        Call instance
      */
     public void setOperationName(QName operationName);
 
@@ -163,10 +169,12 @@ public interface Call {
     public void setPortTypeName(QName portType);
 
     /**
-     * Sets the endpoint address of the target service port. This address must correspond to the transport specified 
-     * in the binding for this Call instance.
+     * Sets the endpoint address of the target service port. This address must
+     * correspond to the transport specified in the binding for this Call
+     * instance.
      *
-     * @param address - Endpoint address of the target service port; specified as URI
+     * @param address - Endpoint address of the target service port; specified
+     *                  as URI
      */
     public void setTargetEndpointAddress(java.net.URL address);
 
@@ -181,18 +189,18 @@ public interface Call {
      * Sets the value for a named property. JAX-RPC 1.0 specification 
      * specifies a standard set of properties that may be passed 
      * to the Call.setProperty method. The properties include:
-     * <UL>
-     * <LI>http.auth.username: Username for the HTTP Basic Authentication
-     * <LI>http.auth.password: Password for the HTTP Basic Authentication
-     * <LI>security.auth.subject: JAAS Subject that carries client 
+     * <ul>
+     * <li>http.auth.username: Username for the HTTP Basic Authentication
+     * <li>http.auth.password: Password for the HTTP Basic Authentication
+     * <li>security.auth.subject: JAAS Subject that carries client 
      * principal and its credentials
-     * <LI>encodingstyle.namespace.uri: Encoding style specified as a 
+     * <li>encodingstyle.namespace.uri: Encoding style specified as a 
      * namespace URI
-     * <LI>soap.http.soapaction.use: Boolean property that indicates
+     * <li>soap.http.soapaction.use: Boolean property that indicates
      * whether or not SOAPAction is to be used
-     * <LI>soap.http.soapaction.uri: Indicates the SOAPAction URI if the 
+     * <li>soap.http.soapaction.uri: Indicates the SOAPAction URI if the 
      * "soap.http.soapaction.use" property is set to true
-     * </UL>
+     * </ul>
      * 
      * @param name - Name of the property
      * @param value - Value of the property
@@ -217,60 +225,72 @@ public interface Call {
 
     // Remote Method Invocation methods
     /**
-     * Invokes a specific operation using a synchronous request-response interaction mode. The invoke method takes 
-     * as parameters the object values corresponding to these defined parameter types. Implementation of the invoke 
-     * method must check whether the passed parameter values correspond to the number, order and types of parameters 
-     * specified in the corresponding operation specification.
+     * Invokes a specific operation using a synchronous request-response
+     * interaction mode. The invoke method takes as parameters the object
+     * values corresponding to these defined parameter types. Implementation of
+     * the invoke method must check whether the passed parameter values
+     * correspond to the number, order and types of parameters specified in the
+     * corresponding operation specification.
      *
      * @param operationName - Name of the operation to invoke
      * @param params  - Parameters for this invocation
      *
      * @return the value returned from the other end. 
      *
-     * @throws java.rmi.RemoteException - if there is any error in the remote method invocation or if the Call 
+     * @throws java.rmi.RemoteException - if there is any error in the remote
+     *                                    method invocation or if the Call 
      * object is not configured properly.
      */
     public Object invoke(QName operationName, Object[] params)
             throws java.rmi.RemoteException;
 
     /**
-     * Invokes a specific operation using a synchronous request-response interaction mode. The invoke method takes 
-     * as parameters the object values corresponding to these defined parameter types. Implementation of the invoke 
-     * method must check whether the passed parameter values correspond to the number, order and types of parameters 
-     * specified in the corresponding operation specification.
+     * Invokes a specific operation using a synchronous request-response
+     * interaction mode. The invoke method takes as parameters the object
+     * values corresponding to these defined parameter types. Implementation of
+     * the invoke method must check whether the passed parameter values
+     * correspond to the number, order and types of parameters specified in the
+     * corresponding operation specification.
      *
      * @param params  - Parameters for this invocation
      *
      * @return the value returned from the other end. 
      *
-     * @throws java.rmi.RemoteException - if there is any error in the remote method invocation or if the Call 
-     * object is not configured properly.
+     * @throws java.rmi.RemoteException - if there is any error in the remote
+     *                                    method invocation or if the Call 
+     *                                    object is not configured properly.
      */
     public Object invoke(Object[] params) throws java.rmi.RemoteException;
 
     /**
-     * Invokes a remote method using the one-way interaction mode. The client thread does not block waiting for the 
-     * completion of the server processing for this remote method invocation. This method must not throw any remote 
-     * exceptions. This method may throw a JAXRPCException during the processing of the one-way remote call.
+     * Invokes a remote method using the one-way interaction mode. The client
+     * thread does not block waiting for the completion of the server
+     * processing for this remote method invocation. This method must not
+     * throw any remote exceptions. This method may throw a JAXRPCException
+     * during the processing of the one-way remote call.
      *
      * @param params - Parameters for this invocation
      *
-     * @throws javax.xml.rpc.JAXRPCException - if there is an error in the configuration of the Call object (example: 
-     * a non-void return type has been incorrectly specified for the one-way call) or if there is any error during 
-     * the invocation of the one-way remote call
+     * @throws javax.xml.rpc.JAXRPCException - if there is an error in the
+     *   configuration of the Call object (example: a non-void return type has
+     *   been incorrectly specified for the one-way call) or if there is any
+     *   error during the invocation of the one-way remote call
      */
     public void invokeOneWay(Object[] params);
 
     /**
-     * This method returns a java.util.Map of {name, value} for the PARAM_MODE_OUT and
-     * PARAM_MODE_INOUT parameters for the last invoked operation.  If there are no output
-     * parameters, this method returns an empty map.  The parameter names in the returned Map are of
-     * type String.  The ty0pe of a value depends on the mapping between the Java and XML types.
+     * This method returns a java.util.Map of {name, value} for the
+     * PARAM_MODE_OUT and PARAM_MODE_INOUT parameters for the last invoked
+     * operation. If there are no output parameters, this method returns an
+     * empty map. The parameter names in the returned Map are of type String.
+     * The type of a value depends on the mapping between the Java and XML
+     * types.
      *
-     * @throws javax.xml.rpc.JAXRPCException - if this method is invoked on a one-way operation or
-     * if it is invoked before any invoke method has been called.
+     * @throws javax.xml.rpc.JAXRPCException - if this method is invoked on a
+     *                                         one-way operation or if it is
+     *                                         invoked before any invoke method
+     *                                         has been called.
      */
     public Map getOutputParams();
 }
-
 
