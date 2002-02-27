58c67d54e72f1613f565b5d35d9e8fc087ec2318
Glen Daniels <gdaniels@apache.org>
2002-02-27 23:00:15 +0000

    Add compound1 implementation

---
+++ test/wsdl/interop3/compound1/SoapInteropCompound1BindingImpl.java
@@ -0,0 +1,19 @@
+/**
+ * SoapInteropCompound1BindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis Wsdl2java emitter.
+ */
+
+package test.wsdl.interop3.compound1;
+
+public class SoapInteropCompound1BindingImpl implements test.wsdl.interop3.compound1.SoapInteropCompound1Binding {
+    public test.wsdl.interop3.compound1.xsd.Person echoPerson(test.wsdl.interop3.compound1.xsd.Person xPerson) throws java.rmi.RemoteException {
+        return xPerson;
+    }
+
+    public test.wsdl.interop3.compound1.xsd.Document echoDocument(test.wsdl.interop3.compound1.xsd.Document xDocument) throws java.rmi.RemoteException {
+        return xDocument;
+    }
+
+}
