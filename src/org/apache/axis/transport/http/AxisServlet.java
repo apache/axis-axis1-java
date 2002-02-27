0c89eb5a7db720b43059f2c312c082dfc3925a41
Glen Daniels <gdaniels@apache.org>
2002-02-27 18:29:35 +0000

    *with* the import statement this time. :)

--- src/org/apache/axis/transport/http/AxisServlet.java
+++ src/org/apache/axis/transport/http/AxisServlet.java
@@ -80,6 +80,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUtils;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
