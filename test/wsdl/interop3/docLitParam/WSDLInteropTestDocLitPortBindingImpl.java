637e656247f564d87fe3a71c2cdee1159b7df00f
Sam Ruby <rubys@apache.org>
2002-02-28 15:15:36 +0000

    Add impls for docLit and rpcEnc

---
+++ test/wsdl/interop3/docLitParam/WSDLInteropTestDocLitPortBindingImpl.java
@@ -0,0 +1,26 @@
+/**
+ * WSDLInteropTestDocLitPortBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis Wsdl2java emitter.
+ */
+
+package test.wsdl.interop3.docLitParam;
+
+public class WSDLInteropTestDocLitPortBindingImpl implements test.wsdl.interop3.docLitParam.WSDLInteropTestDocLitPortBinding {
+    public java.lang.String echoString(java.lang.String param0) throws java.rmi.RemoteException {
+        return param0;
+    }
+
+    public test.wsdl.interop3.docLitParam.xsd.ArrayOfstringLiteral echoStringArray(test.wsdl.interop3.docLitParam.xsd.ArrayOfstringLiteral param0) throws java.rmi.RemoteException {
+        return param0;
+    }
+
+    public test.wsdl.interop3.docLitParam.xsd.SOAPStruct echoStruct(test.wsdl.interop3.docLitParam.xsd.SOAPStruct param0) throws java.rmi.RemoteException {
+        return param0;
+    }
+
+    public void echoVoid() throws java.rmi.RemoteException {
+    }
+
+}
---
+++ test/wsdl/interop3/rpcEnc/WSDLInteropTestRpcEncPortBindingImpl.java
@@ -0,0 +1,26 @@
+/**
+ * WSDLInteropTestRpcEncPortBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis Wsdl2java emitter.
+ */
+
+package test.wsdl.interop3.rpcEnc;
+
+public class WSDLInteropTestRpcEncPortBindingImpl implements test.wsdl.interop3.rpcEnc.WSDLInteropTestRpcEncPortType {
+    public java.lang.String echoString(java.lang.String param0) throws java.rmi.RemoteException {
+        return param0;
+    }
+
+    public java.lang.String[] echoStringArray(java.lang.String[] param0) throws java.rmi.RemoteException {
+        return param0;
+    }
+
+    public test.wsdl.interop3.rpcEnc.xsd.SOAPStruct echoStruct(test.wsdl.interop3.rpcEnc.xsd.SOAPStruct param0) throws java.rmi.RemoteException {
+        return param0;
+    }
+
+    public void echoVoid() throws java.rmi.RemoteException {
+    }
+
+}
