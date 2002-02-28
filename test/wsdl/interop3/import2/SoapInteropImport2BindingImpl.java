91fbcc0579856ae95c2aab044a2feeb348c10776
Sam Ruby <rubys@apache.org>
2002-02-28 14:49:49 +0000

    Add import2 and import3 impls

---
+++ test/wsdl/interop3/import2/SoapInteropImport2BindingImpl.java
@@ -0,0 +1,15 @@
+/**
+ * SoapInteropImport2BindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis Wsdl2java emitter.
+ */
+
+package test.wsdl.interop3.import2;
+
+public class SoapInteropImport2BindingImpl implements test.wsdl.interop3.import2.definitions.SoapInteropImport2PortType {
+    public test.wsdl.interop3.import2.xsd.SOAPStruct echoStruct(test.wsdl.interop3.import2.xsd.SOAPStruct inputStruct) throws java.rmi.RemoteException {
+        return null;
+    }
+
+}
--- test/wsdl/interop3/import3/Import3TestCase.java
+++ test/wsdl/interop3/import3/Import3TestCase.java
@@ -1,6 +1,5 @@
 package test.wsdl.interop3.import3;
 
-import test.wsdl.interop3.import3.step6.definitions.SoapInteropImport3PortTypeServiceLocator;
 
 import test.wsdl.interop3.import3.xsd.SOAPStruct;
 
@@ -105,83 +104,6 @@ public class Import3TestCase extends junit.framework.TestCase {
         }
     }
 
-    public void testStep7EchoStruct() {
-        test.wsdl.interop3.import3.step6.definitions.SoapInteropImport3PortType binding;
-        try {
-            binding = new SoapInteropImport3PortTypeServiceLocator().getSoapInteropImport3Port();
-        }
-        catch (javax.xml.rpc.ServiceException jre) {
-            throw new junit.framework.AssertionFailedError("JAX-RPC ServiceException caught: " + jre);
-        }
-        assertTrue("binding is null", binding != null);
-
-        try {
-            test.wsdl.interop3.import3.step6.xsd.SOAPStruct value = null;
-            value = binding.echoStruct(new test.wsdl.interop3.import3.step6.xsd.SOAPStruct());
-        }
-        catch (java.rmi.RemoteException re) {
-            throw new junit.framework.AssertionFailedError("Remote Exception caught: " + re);
-        }
-    }
-
-    public void testStep7EchoStructArray() {
-        test.wsdl.interop3.import3.step6.definitions.SoapInteropImport3PortType binding;
-        try {
-            binding = new SoapInteropImport3PortTypeServiceLocator().getSoapInteropImport3Port();
-        }
-        catch (javax.xml.rpc.ServiceException jre) {
-            throw new junit.framework.AssertionFailedError("JAX-RPC ServiceException caught: " + jre);
-        }
-        assertTrue("binding is null", binding != null);
-
-        try {
-            test.wsdl.interop3.import3.step6.xsd.SOAPStruct[] value = null;
-            value = binding.echoStructArray(new test.wsdl.interop3.import3.step6.xsd.SOAPStruct[0]);
-        }
-        catch (java.rmi.RemoteException re) {
-            throw new junit.framework.AssertionFailedError("Remote Exception caught: " + re);
-        }
-    }
-
-/* doesn't work yet
-    public void testStep8EchoStruct() {
-        test.wsdl.interop3.import3.step6.definitions.SoapInteropImport3PortType binding;
-        try {
-            binding = new SoapInteropImport3PortTypeServiceLocator().getSoapInteropImport3Port(new java.net.URL("http://mssoapinterop.org/stkV3/wsdl/import3.wsdl"));
-        }
-        catch (Throwable t) {
-            throw new junit.framework.AssertionFailedError("Throwable caught: " + t);
-        }
-        assertTrue("binding is null", binding != null);
-
-        try {
-            test.wsdl.interop3.import3.step6.xsd.SOAPStruct value = null;
-            value = binding.echoStruct(new test.wsdl.interop3.import3.step6.xsd.SOAPStruct());
-        }
-        catch (java.rmi.RemoteException re) {
-            throw new junit.framework.AssertionFailedError("Remote Exception caught: " + re);
-        }
-    }
-
-    public void testStep8EchoStructArray() {
-        test.wsdl.interop3.import3.step6.definitions.SoapInteropImport3PortType binding;
-        try {
-            binding = new SoapInteropImport3PortTypeServiceLocator().getSoapInteropImport3Port(new java.net.URL("http://mssoapinterop.org/stkV3/wsdl/import3.wsdl"));
-        }
-        catch (Throwable t) {
-            throw new junit.framework.AssertionFailedError("Throwable caught: " + t);
-        }
-        assertTrue("binding is null", binding != null);
-
-        try {
-            test.wsdl.interop3.import3.step6.xsd.SOAPStruct[] value = null;
-            value = binding.echoStructArray(new test.wsdl.interop3.import3.step6.xsd.SOAPStruct[0]);
-        }
-        catch (java.rmi.RemoteException re) {
-            throw new junit.framework.AssertionFailedError("Remote Exception caught: " + re);
-        }
-    }
-*/
 
     public static void main(String[] args) {
         junit.textui.TestRunner.run(new junit.framework.TestSuite(Import3TestCase.class));
