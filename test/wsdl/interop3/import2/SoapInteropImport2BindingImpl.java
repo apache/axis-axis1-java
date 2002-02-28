dbc4b740e0898c4f2e9692418ab0499c821e5fee
Sam Ruby <rubys@apache.org>
2002-02-28 17:40:19 +0000

    Actually return the desired value

--- test/wsdl/interop3/import2/SoapInteropImport2BindingImpl.java
+++ test/wsdl/interop3/import2/SoapInteropImport2BindingImpl.java
@@ -9,7 +9,7 @@ package test.wsdl.interop3.import2;
 
 public class SoapInteropImport2BindingImpl implements test.wsdl.interop3.import2.definitions.SoapInteropImport2PortType {
     public test.wsdl.interop3.import2.xsd.SOAPStruct echoStruct(test.wsdl.interop3.import2.xsd.SOAPStruct inputStruct) throws java.rmi.RemoteException {
-        return null;
+        return inputStruct;
     }
 
 }
