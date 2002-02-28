fd68a1be857876cd9a989f0b276c04aa65917a5b
Sam Ruby <rubys@apache.org>
2002-02-28 15:26:27 +0000

    Add compound2 and docLit impls

---
+++ test/wsdl/interop3/compound2/SoapInteropCompound2BindingImpl.java
@@ -0,0 +1,15 @@
+/**
+ * SoapInteropCompound2BindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis Wsdl2java emitter.
+ */
+
+package test.wsdl.interop3.compound2;
+
+public class SoapInteropCompound2BindingImpl implements test.wsdl.interop3.compound2.SoapInteropCompound2Binding {
+    public org.soapinterop.Employee echoEmployee(org.soapinterop.Employee xEmployee) throws java.rmi.RemoteException {
+        return xEmployee;
+    }
+
+}
