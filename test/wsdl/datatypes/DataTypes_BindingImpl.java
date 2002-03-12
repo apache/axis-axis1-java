1cab109c47d08ce0f8cdc6b608894609a0684813
Davanum Srinivas <dims@apache.org>
2002-03-12 16:13:37 +0000

    Equivalent for DataTypes Sample in .NET Framework SDK

---
+++ test/wsdl/datatypes/DataTypes_BindingImpl.java
@@ -0,0 +1,55 @@
+/**
+ * DataTypesSoapImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis Wsdl2java emitter.
+ */
+
+package test.wsdl.datatypes;
+
+public class DataTypes_BindingImpl implements DataTypes_Binding {
+    public java.lang.String sayHello() throws java.rmi.RemoteException {
+        return "Hello World!";
+    }
+
+    public java.lang.String sayHelloName(java.lang.String name) throws java.rmi.RemoteException {
+        return "Hello " + name;
+    }
+
+    public ArrayOfInt getIntArray() throws java.rmi.RemoteException {
+        int[] a = new int[5];
+        for (int i=0; i<5; i++)
+            a[i] = i*10;
+        ArrayOfInt array = new ArrayOfInt();
+        array.set_int(a);
+        return array;
+    }
+
+    public Mode getMode() throws java.rmi.RemoteException {
+        return new Mode(Mode._Off);
+    }
+
+    public Order getOrder() throws java.rmi.RemoteException {
+        Order myOrder = new Order();
+
+         myOrder.setPrice(34.5);
+         myOrder.setOrderID(323232);
+
+         return myOrder;
+    }
+
+    public ArrayOfOrder getOrders() throws java.rmi.RemoteException {
+        Order [] myOrders = new Order[2];
+
+        myOrders[0] = new Order();
+        myOrders[0].setPrice(34.5);
+        myOrders[0].setOrderID(323232);
+        myOrders[1] = new Order();
+        myOrders[1].setPrice(99.4);
+        myOrders[1].setOrderID(645645);
+        ArrayOfOrder array = new ArrayOfOrder();
+        array.setOrder(myOrders);
+        return array;
+    }
+
+}
