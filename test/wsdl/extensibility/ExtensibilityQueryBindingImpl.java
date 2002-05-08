b8fd090817831ceac5c7bea07a01c95d6b526e83
Davanum Srinivas <dims@apache.org>
2002-05-08 13:04:46 +0000

    Patch for "updated the extensibility test to handle arrays" from "Thomas Sandholm" <sandholm@mcs.anl.gov>

--- test/wsdl/extensibility/ExtensibilityQueryBindingImpl.java
+++ test/wsdl/extensibility/ExtensibilityQueryBindingImpl.java
@@ -9,22 +9,30 @@ import java.util.Calendar;
 import javax.xml.rpc.namespace.QName;
 
 public class ExtensibilityQueryBindingImpl implements ExtensibilityQueryBinding {
+    private final static String[] books = new String[] { "The Grid", "The Oxford Dictionary" }; 
+    private final static String[] subjects = new String[] { "Computer Science", "English" }; 
 
     public ExtensibilityType query(ExtensibilityType query) throws java.rmi.RemoteException {
         ExtensibilityType result = new ExtensibilityType();
         Object obj = query.getAny();
-        if (obj instanceof BookQuery) {
-            System.out.println("ExtensibilityQueryBindingImpl: Found book");
-            BookQuery bookQuery = (BookQuery) obj;
+        if (obj instanceof BookType) {
+            BookType bookQuery = (BookType) obj;
             String subject = bookQuery.getSubject();
-            QueryResultType queryResultType = new QueryResultType();
-            QueryResult queryResult = new QueryResult(); 
-            queryResult.setName(subject);
-            queryResult.setValue("The Grid");
-            queryResult.setTime(Calendar.getInstance());
-            queryResult.setQueryType(new QName("urn:QueryType","BookQuery"));
-            queryResultType.setQueryResult(queryResult);
-            result.setAny(queryResultType);
+            System.out.println("ExtensibilityQueryBindingImpl: Found book subject query " + subject);
+  
+            QueryResultElement resultElement = new QueryResultElement();
+            ResultListType resultList = new ResultListType();
+            resultElement.setResultList(resultList);
+            QueryResultType[] queryResult = new QueryResultType[books.length];
+            for (int i = 0; i < books.length; i++) {
+                queryResult[i] = new QueryResultType();
+                queryResult[i].setName(subjects[i]);
+                queryResult[i].setValue(books[i]);
+                queryResult[i].setTime(Calendar.getInstance());
+                queryResult[i].setQueryType(new QName("urn:QueryType","BookQuery"));
+            }
+            resultList.setResult(queryResult);
+            result.setAny(resultElement);
         }
         return result;
     }
