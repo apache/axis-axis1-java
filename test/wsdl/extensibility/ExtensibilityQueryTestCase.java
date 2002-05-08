87d3b068f9fb250a10a27514b08315858d572145
Davanum Srinivas <dims@apache.org>
2002-05-08 13:04:46 +0000

    Patch for "updated the extensibility test to handle arrays" from "Thomas Sandholm" <sandholm@mcs.anl.gov>

--- test/wsdl/extensibility/ExtensibilityQueryTestCase.java
+++ test/wsdl/extensibility/ExtensibilityQueryTestCase.java
@@ -39,49 +39,54 @@ public class ExtensibilityQueryTestCase extends junit.framework.TestCase {
         assertTrue("binding is null", binding != null);
 
         try {
-           
             ExtensibilityType expression = new ExtensibilityType(); 
-            FindBooksQueryExpressionType bookQuery = new FindBooksQueryExpressionType(); 
-            BookQuery book = new BookQuery();
-            book.setSubject("Computer Science");
+            FindBooksQueryExpressionElement bookQuery = new FindBooksQueryExpressionElement(); 
+            BookType book = new BookType();
+            book.setSubject("all");
             bookQuery.setBookQuery(book);
             expression.setAny(bookQuery); 
             ExtensibilityType any = binding.query(expression);
-            QueryResult result = (QueryResult) any.getAny();
-            System.out.println("ExtensibilityQueryTestCase: Result name: " + result.getName() + " value: " + result.getValue() +
-                               " date: " + result.getTime().getTime() + " type: " + result.getQueryType());
-            assertTrue(result.getName().equals("Computer Science"));
-            assertTrue(result.getValue().equals("The Grid"));
-            assertTrue(result.getTime().before(Calendar.getInstance()));
-            assertTrue(result.getQueryType().getNamespaceURI().equals("urn:QueryType"));
-            assertTrue(result.getQueryType().getLocalPart().equals("BookQuery"));
+            ResultListType result = (ResultListType) any.getAny();
+            QueryResultType[] queryResult = result.getResult();
+            assertTrue(queryResult.length == 2); 
+            isValid(queryResult[0], "Computer Science", "The Grid"); 
+            isValid(queryResult[1], "English", "The Oxford Dictionary"); 
         }
         catch (java.rmi.RemoteException re) {
             throw new junit.framework.AssertionFailedError("Remote Exception caught: " + re);
         }
     }
 
+    private void isValid(QueryResultType result, String name, String value) {
+        System.out.println("ExtensibilityQueryTestCase: Result name: [" + result.getName() + "] value: [" + 
+                           result.getValue() + "] date: [" + result.getTime().getTime() + "] type: [" + 
+                           result.getQueryType() + "]");
+                               
+        assertTrue(result.getName().equals(name));
+        assertTrue(result.getValue().equals(value));
+        assertTrue(result.getTime().before(Calendar.getInstance()));
+        assertTrue(result.getQueryType().getNamespaceURI().equals("urn:QueryType"));
+        assertTrue(result.getQueryType().getLocalPart().equals("BookQuery"));
+    }
+
     private void addDynamicTypeMappings(AxisEngine engine) throws Exception {
         TypeMappingRegistry registry = engine.getTypeMappingRegistry(); 
         TypeMapping mapping = registry.createTypeMapping();
-        QName queryQname = new QName("urn:QueryTypes", "FindBooksQueryExpressionType");
-        mapping.register(FindBooksQueryExpressionType.class,
-                         queryQname,
-                         new BeanSerializerFactory(FindBooksQueryExpressionType.class, queryQname),
-                         new BeanDeserializerFactory(FindBooksQueryExpressionType.class, queryQname));
-        QName bookQname = new QName("urn:QueryTypes", "BookQuery");
-        mapping.register(BookQuery.class,
-                         bookQname,
-                         new BeanSerializerFactory(BookQuery.class, bookQname),
-                         new BeanDeserializerFactory(BookQuery.class, bookQname));
-        QName resultQname = new QName("urn:QueryTypes", "queryResult");
-        mapping.register(QueryResult.class,
-                         resultQname,
-                         new BeanSerializerFactory(QueryResult.class, resultQname),
-                         new BeanDeserializerFactory(QueryResult.class, resultQname));
+        addBeanMapping(mapping, "FindBooksQueryExpressionElement", FindBooksQueryExpressionElement.class);
+        addBeanMapping(mapping, "BookType", BookType.class);
+        addBeanMapping(mapping, "resultList", ResultListType.class);
+        addBeanMapping(mapping, "result", QueryResultType.class);
         registry.register("",mapping);
         EngineConfiguration config = engine.getConfig();
         config.writeEngineConfig(engine);
+    }
+
+    private void addBeanMapping(TypeMapping mapping, String localName, Class javaClass) {
+        QName qname = new QName("urn:QueryTypes", localName);
+        mapping.register(javaClass,
+                         qname,
+                         new BeanSerializerFactory(javaClass, qname),
+                         new BeanDeserializerFactory(javaClass, qname));
     }
 
     private void deployServer() {
