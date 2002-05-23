0a9161e1a9bec5e4c1aeaf46d838fb3db3103b22
Sam Ruby <rubys@apache.org>
2002-05-23 11:24:22 +0000

    Get interop3 to build again

--- test/wsdl/interop3/docLit/WSDLInteropTestDocLitPortBindingImpl.java
+++ test/wsdl/interop3/docLit/WSDLInteropTestDocLitPortBindingImpl.java
@@ -7,7 +7,7 @@
 
 package test.wsdl.interop3.docLit;
 
-public class WSDLInteropTestDocLitPortBindingImpl implements test.wsdl.interop3.docLit.WSDLInteropTestDocLitPortBinding {
+public class WSDLInteropTestDocLitPortBindingImpl implements test.wsdl.interop3.docLit.WSDLInteropTestDocLitPortType {
     public java.lang.String echoString(java.lang.String echoStringParam) throws java.rmi.RemoteException {
         return echoStringParam;
     }
