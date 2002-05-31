af1e8338db1d3ac8817d881185092ec2ccaa9800
Davanum Srinivas <dims@apache.org>
2002-05-31 20:24:31 +0000

    - Add DelayedStockQuote sample (Has a problem - getValue() returns null @ line 114) - Add a "TODO:" Tag in MessageContext.java

---
+++ samples/jaxm/DelayedStockQuote.java
@@ -0,0 +1,121 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Axis" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+//package samples.jaxm;
+
+import javax.xml.messaging.URLEndpoint;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPConnection;
+import javax.xml.soap.SOAPConnectionFactory;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import java.util.Iterator;
+
+public class DelayedStockQuote {
+    public static void main(String[] args) throws Exception {
+        DelayedStockQuote stockQuote = new DelayedStockQuote();
+        System.out.print("The last price for SUNW is " + stockQuote.getStockQuote("SUNW"));
+    }
+
+    public String getStockQuote(String tickerSymbol) throws Exception {
+        SOAPConnectionFactory scFactory = SOAPConnectionFactory.newInstance();
+        SOAPConnection con = scFactory.createConnection();
+
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage message = factory.createMessage();
+
+        SOAPPart soapPart = message.getSOAPPart();
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+
+        SOAPHeader header = envelope.getHeader();
+        SOAPBody body = envelope.getBody();
+
+        header.detachNode();
+
+        Name bodyName = envelope.createName("getQuote", "n", "urn:xmethods-delayed-quotes");
+        SOAPBodyElement gltp = body.addBodyElement(bodyName);
+
+        Name name = envelope.createName("symbol");
+        SOAPElement symbol = gltp.addChildElement(name);
+        symbol.addTextNode(tickerSymbol);
+
+        URLEndpoint endpoint = new URLEndpoint("http://66.28.98.121:9090/soap");
+        SOAPMessage response = con.call(message, endpoint);
+        con.close();
+
+        SOAPPart sp = response.getSOAPPart();
+        SOAPEnvelope se = sp.getEnvelope();
+        SOAPBody sb = se.getBody();
+        Iterator it = sb.getChildElements();
+        while (it.hasNext()) {
+            SOAPBodyElement bodyElement = (SOAPBodyElement) it.next();
+            Iterator it2 = bodyElement.getChildElements();
+            while (it2.hasNext()) {
+                SOAPElement element2 = (SOAPElement) it2.next();
+                System.out.println(((org.apache.axis.message.MessageElement)element2).toString());
+                // TODO: Find out why getValue returns null.
+                String lastPrice = element2.getValue();
+                return lastPrice;
+            }
+        }
+        return null;
+    }
+}
+
