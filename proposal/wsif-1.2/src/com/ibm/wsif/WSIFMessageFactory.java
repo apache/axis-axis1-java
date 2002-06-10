261b5f5ff75cc7115963471066ff736b89d67416
Davanum Srinivas <dims@apache.org>
2002-06-10 13:14:27 +0000

    Cleaning up the proposal directory. Now that xml-axis-wsif cvs has been populated - http://cvs.apache.org/viewcvs.cgi/xml-axis-wsif/

--- proposal/wsif-1.2/src/com/ibm/wsif/WSIFMessageFactory.java
+++
@@ -1,9 +0,0 @@
-// (C) Copyright IBM Corp. 2001, 2002  All Rights Reserved.
-package com.ibm.wsif;
-
-public interface WSIFMessageFactory {
-
-   public WSIFMessage createMessage();
-   public WSIFMessage createMessage(String ns, String n);
-   
-}
