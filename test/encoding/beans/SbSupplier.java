3532a83ffa5e48e5c08298bbcbf9b01678a0bbb1
Davanum Srinivas <dims@apache.org>
2002-06-28 12:36:51 +0000

    Added a test for Bug 10300 - Bean deserialize for complex types does not work

---
+++ test/encoding/beans/SbSupplier.java
@@ -0,0 +1,9 @@
+package test.encoding.beans;
+
+public class SbSupplier implements java.io.Serializable
+{
+    public SbSupplier(){}
+    public Integer searchType;
+    public String  supplierCode;
+    public String  chanNel;
+}
