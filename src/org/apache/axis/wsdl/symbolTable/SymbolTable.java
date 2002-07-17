30503d85439fafaace48f9b40720ecccdcb82fc2
Russell Butek <butek@apache.org>
2002-07-17 17:32:56 +0000

    If we have
    <message name="in"><part name="in" element="e"/></message>
    <message name="out"><part name="out" element="e"/></message>
    
    where e contains a string named a.  Note that the input and output
    messages both refer to the same element.  Because the unwrapped list
    of parameters are named the same for input and output, we used to say
    we had an inout parameter and generate the following:
    
    public void e(StringHolder a);
    
    but inoutness is determined by part name, not parameter name, so we
    should generate:
    
    public String e(String a);
    
    This is a TCK issue.

--- src/org/apache/axis/wsdl/symbolTable/SymbolTable.java
+++ src/org/apache/axis/wsdl/symbolTable/SymbolTable.java
@@ -83,11 +83,12 @@ import javax.wsdl.Output;
 import javax.wsdl.Part;
 import javax.wsdl.Port;
 import javax.wsdl.PortType;
-import javax.xml.namespace.QName;
 import javax.wsdl.Service;
 import javax.wsdl.WSDLException;
 
 import javax.wsdl.factory.WSDLFactory;
+
+import javax.xml.namespace.QName;
 
 import javax.wsdl.xml.WSDLReader;
 
@@ -1102,7 +1103,9 @@ public class SymbolTable {
         // At this point we know the name and type of the parameter, and that it's at least an
         // in parameter.  Now check to see whether it's also in the outputs Vector.  If it is,
         // then it's an inout parameter.
-        if (outdex >= 0) {
+        // Don't bother doing this if the parameters are wrapped since their
+        // names won't be the part names.
+        if (outdex >= 0 && !wrapped) {
             Parameter outParam = (Parameter)outputs.get(outdex);
             if (p.getType().equals(outParam.getType())) {
                 outputs.remove(outdex);
@@ -1299,7 +1302,7 @@ public class SymbolTable {
                 v.add(p);
             }
         } // while
-        
+
     } // partStrings
 
     /**
