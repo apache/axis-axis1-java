ad77ccc4ab4b233bed8c27e626acbb7b1134057f
Davanum Srinivas <dims@apache.org>
2002-07-24 17:13:21 +0000

    Fix for 11106 - CalendarDeserializer is not thread safe

--- src/org/apache/axis/encoding/ser/CalendarDeserializer.java
+++ src/org/apache/axis/encoding/ser/CalendarDeserializer.java
@@ -73,7 +73,7 @@ import java.util.TimeZone;
  */
 public class CalendarDeserializer extends SimpleDeserializer {
 
-    private static SimpleDateFormat zulu = 
+    private static SimpleDateFormat zulu =
         new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
                           //  0123456789 0 123456789
 
@@ -82,7 +82,7 @@ public class CalendarDeserializer extends SimpleDeserializer {
     }
 
     /**
-     * The Deserializer is constructed with the xmlType and 
+     * The Deserializer is constructed with the xmlType and
      * javaType
      */
     public CalendarDeserializer(Class javaType, QName xmlType) {
@@ -97,50 +97,52 @@ public class CalendarDeserializer extends SimpleDeserializer {
         Calendar calendar = Calendar.getInstance();
         Date date;
         boolean bc = false;
-        
+
         // validate fixed portion of format
         if ( source != null ) {
             if (source.charAt(0) == '+')
                 source = source.substring(1);
-            
+
             if (source.charAt(0) == '-') {
                 source = source.substring(1);
                 bc = true;
             }
-            
-            if (source.length() < 19) 
+
+            if (source.length() < 19)
                 throw new NumberFormatException(
                            JavaUtils.getMessage("badDateTime00"));
-    
+
             if (source.charAt(4) != '-' || source.charAt(7) != '-' ||
                 source.charAt(10) != 'T')
                 throw new NumberFormatException(
                                                 JavaUtils.getMessage("badDate00"));
-            
+
             if (source.charAt(13) != ':' || source.charAt(16) != ':')
                 throw new NumberFormatException(
                                                 JavaUtils.getMessage("badTime00"));
         }
-        
+
         // convert what we have validated so far
         try {
-            date = zulu.parse(source == null ? null :
-                                (source.substring(0,19)+".000Z") );
+            synchronized (zulu) {
+                date = zulu.parse(source == null ? null :
+                                    (source.substring(0,19)+".000Z") );
+            }
         } catch (Exception e) {
             throw new NumberFormatException(e.toString());
         }
-        
+
         int pos = 19;
-        
+
         // parse optional milliseconds
         if ( source != null ) {
             if (pos < source.length() && source.charAt(pos)=='.') {
                 int milliseconds = 0;
                 int start = ++pos;
-                while (pos<source.length() && 
+                while (pos<source.length() &&
                        Character.isDigit(source.charAt(pos)))
                     pos++;
-                
+
                 String decimal=source.substring(start,pos);
                 if (decimal.length()==3) {
                     milliseconds=Integer.parseInt(decimal);
@@ -151,45 +153,45 @@ public class CalendarDeserializer extends SimpleDeserializer {
                     milliseconds=Integer.parseInt(decimal.substring(0,3));
                     if (decimal.charAt(3)>='5') ++milliseconds;
                 }
-                
+
                 // add milliseconds to the current date
                 date.setTime(date.getTime()+milliseconds);
             }
-            
+
             // parse optional timezone
             if (pos+5 < source.length() &&
                 (source.charAt(pos)=='+' || (source.charAt(pos)=='-')))
                 {
-                    if (!Character.isDigit(source.charAt(pos+1)) || 
-                        !Character.isDigit(source.charAt(pos+2)) || 
-                        source.charAt(pos+3) != ':'              || 
-                        !Character.isDigit(source.charAt(pos+4)) || 
+                    if (!Character.isDigit(source.charAt(pos+1)) ||
+                        !Character.isDigit(source.charAt(pos+2)) ||
+                        source.charAt(pos+3) != ':'              ||
+                        !Character.isDigit(source.charAt(pos+4)) ||
                         !Character.isDigit(source.charAt(pos+5)))
                         throw new NumberFormatException(
                                                         JavaUtils.getMessage("badTimezone00"));
-                    
+
                     int hours = (source.charAt(pos+1)-'0')*10
                         +source.charAt(pos+2)-'0';
                     int mins  = (source.charAt(pos+4)-'0')*10
                         +source.charAt(pos+5)-'0';
                     int milliseconds = (hours*60+mins)*60*1000;
-                    
+
                     // subtract milliseconds from current date to obtain GMT
                     if (source.charAt(pos)=='+') milliseconds=-milliseconds;
                     date.setTime(date.getTime()+milliseconds);
-                    pos+=6;  
+                    pos+=6;
                 }
-            
+
             if (pos < source.length() && source.charAt(pos)=='Z') {
                 pos++;
                 calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
             }
-            
+
             if (pos < source.length())
                 throw new NumberFormatException(
                                                 JavaUtils.getMessage("badChars00"));
         }
-        
+
         calendar.setTime(date);
 
         // support dates before the Christian era
