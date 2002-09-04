47878d93b5b50bc28020ee3d9819b813048fff75
Davanum Srinivas <dims@apache.org>
2002-09-04 14:55:30 +0000

    Patch from "Greg Truty" <gtruty@us.ibm.com> for JAXRPC Handler testcase + changes.
    Notes from Greg:
    I've made a bit of progress w/the Handler code and have testcase plus some
    code changes (along w/some other bugs that I have noticed).  This is on a
    version of todays driver (9/03).
    
    The testcase is included below in test\jaxrpchandler.  It tests the
    addition of a client/server side handler, along w/ client-config and
    programatic API access changes.  It deploys/undeploys the service
    appropriately.  The handler adds a header element at each of the hook
    points, incrementing a counter in the header.  Lastly, on the client
    response, it checks to ensure the counter has been updated appropriate and
    calls a static method to the testcase (it's out-of-band) to confirm that
    the handlers executed appropriately.
    
    The code changes take into account the role/header/parameter options (which
    were wrong/missing in the code orginally checked in).

---
+++ test/wsdl/jaxrpchandler/JAXRPCHandlerTestCase.java
@@ -0,0 +1,111 @@
+package test.wsdl.jaxrpchandler;
+
+import junit.framework.TestCase;
+
+import org.apache.axis.client.AdminClient;
+import org.apache.axis.utils.Admin;
+import javax.xml.rpc.ParameterMode;
+import javax.xml.namespace.QName;
+import javax.xml.rpc.handler.HandlerRegistry;
+import javax.xml.rpc.handler.HandlerInfo;
+
+import org.apache.axis.AxisFault;
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import org.apache.axis.encoding.XMLType;
+import org.apache.axis.utils.Options;
+
+import java.net.URL;
+
+/**
+ *
+ */
+public class JAXRPCHandlerTestCase extends TestCase {
+
+	private static boolean _roundtrip = false;
+	public static void completeRoundtrip() {
+		_roundtrip = true;
+	}
+
+	/**
+	 * Default constructor for use as service
+	 */
+	public JAXRPCHandlerTestCase() {
+		super("JAXRPCHandlerTest");
+	}
+
+	public JAXRPCHandlerTestCase(String name) {
+		super(name);
+	}
+
+	public void testStockQuote() throws Exception {
+	    String[] args = {};
+	    goStockQuote(args);
+	}
+
+	public void goStockQuote(String[] args) throws Exception {
+		Options opts = new Options( args );
+		args = opts.getRemainingArgs();
+
+		URL url = new URL(opts.getURL());
+		String user = opts.getUser();
+		String passwd = opts.getPassword();
+		System.out.println("URL is " + url);
+
+		_roundtrip = false;
+		doTestDeploy();
+		float val = getQuote(url,false);
+		assertEquals("Stock price is not the expected 55.25 +/- 0.01", val, 55.25, 0.01);
+		assertTrue("Expected setting for config-based handlers should be true", _roundtrip == true);
+		doTestClientUndeploy();
+		_roundtrip = false;
+		val = getQuote(url,true);
+		assertEquals("Stock price is not the expected 55.25 +/- 0.01", val, 55.25, 0.01);
+		assertTrue("Expected setting for programmatic-based handlers should be true", _roundtrip == true);
+		doTestServerUndeploy();
+
+	}
+
+	public float getQuote (URL url, boolean runJAXRPCHandler) throws Exception {
+		StockQuoteService  service = new StockQuoteServiceLocator();
+		if (runJAXRPCHandler) {
+			HandlerRegistry hr = service.getHandlerRegistry();
+			java.util.List lhi = new java.util.Vector();
+			test.wsdl.jaxrpchandler.ClientHandler mh = new test.wsdl.jaxrpchandler.ClientHandler(); 
+			Class myhandler = mh.getClass();
+			HandlerInfo hi = new HandlerInfo(myhandler,null,null);
+			lhi.add(hi);
+			hr.setHandlerChain(new QName("","jaxrpchandler"),lhi);
+		}
+
+		float res;
+
+		StockQuote sq = service.getjaxrpchandler(url);
+		res = sq.getQuote("XXX");
+
+		return res;
+	}
+
+	public static void main(String[] args) throws Exception {
+		JAXRPCHandlerTestCase test = new JAXRPCHandlerTestCase("test");
+		test.goStockQuote(args);
+	}
+
+	public void doTestClientUndeploy() throws Exception {
+		String[] args1 = { "client", "test/wsdl/jaxrpchandler/undeploy.wsdd"};
+		Admin.main(args1);
+	}
+
+	public void doTestServerUndeploy() throws Exception {
+		String[] args = { "test/wsdl/jaxrpchandler/undeploy.wsdd"};
+		AdminClient.main(args);
+	}
+
+	public void doTestDeploy() throws Exception {
+		String[] args = { "test/wsdl/jaxrpchandler/server_deploy.wsdd"};
+		AdminClient.main(args);
+		String[] args1 = { "client", "test/wsdl/jaxrpchandler/client_deploy.wsdd"};
+		Admin.main(args1);
+	}
+
+}
---
+++ test/wsdl/jaxrpchandler/ServerHandler.java
@@ -0,0 +1,99 @@
+package test.wsdl.jaxrpchandler;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.handler.Handler;
+import javax.xml.rpc.handler.HandlerInfo;
+import javax.xml.rpc.handler.MessageContext;
+import javax.xml.rpc.handler.soap.SOAPMessageContext;
+import javax.xml.soap.*;
+import java.util.*;
+
+/**
+ */
+public class ServerHandler implements Handler {
+
+	private int _counter = 0;
+	public final static String _actorURI = "myActorURI";
+	/**
+	 * Constructor for ClientHandler.
+	 */
+	public ServerHandler() {
+		super();
+	}
+
+	/**
+	 * @see javax.xml.rpc.handler.Handler#handleRequest(MessageContext)
+	 */
+	public boolean handleRequest(MessageContext context) {
+		System.out.println("Hey - in Handle request");
+		try {
+			SOAPMessageContext smc = (SOAPMessageContext) context;
+			SOAPMessage msg = smc.getMessage();
+			SOAPPart sp = msg.getSOAPPart();
+			SOAPEnvelope se = sp.getEnvelope();
+			SOAPHeader sh = se.getHeader();
+			Name name = se.createName("HeaderTest", "AXIS",
+									  "http://xml.apache.org/axis");
+			Iterator iter = sh.extractHeaderElements(_actorURI);
+			while (iter.hasNext()) {
+				SOAPHeaderElement she = (SOAPHeaderElement) iter.next();
+				String counter = she.getAttributeValue(se.createName("counter","",""));
+				_counter = Integer.parseInt(counter) + 1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	/**
+	 * @see javax.xml.rpc.handler.Handler#handleResponse(MessageContext)
+	 */
+	public boolean handleResponse(MessageContext context) {
+
+		System.out.println("Hey - in Handle response");
+		try {
+			SOAPMessageContext smc = (SOAPMessageContext) context;
+			SOAPMessage msg = smc.getMessage();
+			SOAPPart sp = msg.getSOAPPart();
+			SOAPEnvelope se = sp.getEnvelope();
+			SOAPHeader sh = se.getHeader();
+			Name name = se.createName("HeaderTest", "AXIS",
+									  "http://xml.apache.org/axis");
+			SOAPHeaderElement she = sh.addHeaderElement(name);
+			she.addAttribute(se.createName("counter","",""), new Integer(_counter +1).toString());
+			she.setActor(_actorURI);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	/**
+	 * @see javax.xml.rpc.handler.Handler#handleFault(MessageContext)
+	 */
+	public boolean handleFault(MessageContext context) {
+		return false;
+	}
+
+	/**
+	 * @see javax.xml.rpc.handler.Handler#init(HandlerInfo)
+	 */
+	public void init(HandlerInfo config) {
+	}
+
+	/**
+	 * @see javax.xml.rpc.handler.Handler#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see javax.xml.rpc.handler.Handler#getHeaders()
+	 */
+	public QName[] getHeaders() {
+		return null;
+	}
+
+}
