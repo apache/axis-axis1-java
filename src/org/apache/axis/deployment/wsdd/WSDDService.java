602ee489343a1541adf5d8e57ef6b947b0befab5
Davanum Srinivas <dims@apache.org>
2002-09-18 18:25:50 +0000

    Possible fix for Bug 12737 - reference to AxisServer fails

--- src/org/apache/axis/deployment/wsdd/WSDDService.java
+++ src/org/apache/axis/deployment/wsdd/WSDDService.java
@@ -538,7 +538,7 @@ public class WSDDService
             }
             tm.register( mapping.getLanguageSpecificType(), mapping.getQName(), ser, deser);
         } catch (ClassNotFoundException e) {
-            throw new WSDDException(e);
+            log.info(Messages.getMessage("unabletoDeployTypemapping00", mapping.getQName().toString()), e);
         } catch (Exception e) {
             throw new WSDDException(e);
         }
