8da457114d77f04496b5b47aa990a22bcf323289
Davanum Srinivas <dims@apache.org>
2002-09-26 11:51:37 +0000

    Adding test case for 12753 - problem while deserializing output params based on WSDL (3 in params, 2 out params) in the client; org.xml.sax.SAXException: Bad types (double -> class java.lang.String)
    Note:
    Seems like the fix for 12626 fixed this as well.

--- test/wsdl/esr/EsrTestBindingImpl.java
+++ test/wsdl/esr/EsrTestBindingImpl.java
@@ -11,4 +11,12 @@ public class EsrTestBindingImpl implements test.wsdl.esr.EsrTest{
         echoVal.value = (short)value;
         sqrtVal.value = Math.sqrt(value);
     }
+    public void esrInOut2(java.lang.String bstrSAH, java.lang.String bstrSUH, short value, javax.xml.rpc.holders.ShortHolder echoVal, javax.xml.rpc.holders.DoubleHolder sqrtVal) throws java.rmi.RemoteException
+    {
+        echoVal.value = value;
+        sqrtVal.value = Math.sqrt( (double) value );
+        if(Double.isNaN(sqrtVal.value)) {
+          throw org.apache.axis.AxisFault.makeFault(new Exception("arg cannot be < 0"));
+        }
+    }
 }
