be995570045e922f58a84a0c7fab35429c805fa2
Russell Butek <butek@apache.org>
2002-10-11 13:39:53 +0000

    I added an implicit fault to the header test case to make sure implicit faults are generated.

---
+++ test/wsdl/header/VerifyFilesTestCase.java
@@ -0,0 +1,107 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Axis" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+/**
+ * This tests the file generation of only the items that are referenced in WSDL
+ * 
+ */ 
+package test.wsdl.header;
+
+import java.io.File;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import test.wsdl.filegen.FileGenTestCase;
+
+public class VerifyFilesTestCase extends FileGenTestCase {
+    public VerifyFilesTestCase(String name) {
+        super(name);
+    }
+
+    /**
+     * List of files which should be generated.  We're primarily concerned with
+     * the implicit fault class.
+     */
+    protected Set shouldExist() {
+        HashSet set = new HashSet();
+
+        set.add("BindingImpl.java");
+        set.add("BindingSkeleton.java");
+        set.add("BindingStub.java");
+        set.add("HeaderService.java");
+        set.add("HeaderServiceLocator.java");
+        set.add("HeaderServiceTestCase.java");
+        set.add("HeaderType.java");
+        set.add("ImplicitFault.java");
+        set.add("Op1Fault.java");
+        set.add("PortType.java");
+        set.add("VerifyFilesTestCase.java");
+        set.add("deploy.wsdd");
+        set.add("undeploy.wsdd");
+        return set;
+    } // shouldExist
+
+    /**
+    /**
+     * The directory containing the files that should exist.
+     */
+    protected String rootDir() {
+        return "build" + File.separator + "work" + File.separator + 
+                "test" + File.separator + "wsdl" + File.separator +
+                "header";
+    } // rootDir
+
+} // class VerifyFilesTestCase
