ba7ba641378bb07383c81aede27cfa623acc87c4
Davanum Srinivas <dims@apache.org>
2002-11-30 21:38:47 +0000

    Fix for  Bug 13262 - WSDL Generation broken

--- src/org/apache/axis/providers/java/JavaProvider.java
+++ src/org/apache/axis/providers/java/JavaProvider.java
@@ -425,6 +425,9 @@ public abstract class JavaProvider extends BasicProvider
 
             Emitter emitter = new Emitter();
 
+            // Set the name for the target service.
+            //emitter.setServiceElementName(serviceDesc.getName());
+            
             // service alias may be provided if exact naming is required,
             // otherwise Axis will name it according to the implementing class name
             String alias = (String)service.getOption("alias");
