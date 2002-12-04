3d8786769a5abb0f9d941d018e14f5d3ac349def
sandolm <dev-null@apache.org>
2002-12-04 20:06:59 +0000

    patch to allow any elements to be passed in exceptions + extension of wsdl fault test case

--- test/wsdl/faults/FaultServiceSoapBindingImpl.java
+++ test/wsdl/faults/FaultServiceSoapBindingImpl.java
@@ -8,6 +8,9 @@
 
 package test.wsdl.faults;
 
+import org.apache.axis.utils.XMLUtils;
+import org.apache.axis.message.MessageElement;
+
 public class FaultServiceSoapBindingImpl implements test.wsdl.faults.FaultServicePortType {
     public float getQuote(java.lang.String tickerSymbol) throws java.rmi.RemoteException, InvalidTickerFaultMessage {
         throw new InvalidTickerFaultMessage(tickerSymbol);
@@ -16,4 +19,15 @@ public class FaultServiceSoapBindingImpl implements test.wsdl.faults.FaultServic
     public int throwFault(int a, java.lang.String b, float c) throws java.rmi.RemoteException, test.wsdl.faults.DerivedFault {
         throw new DerivedFault2(a, b, c);
     }
+
+    public int throwExtensionFault(java.lang.String description) throws java.rmi.RemoteException, test.wsdl.faults.ExtensionFault {
+        ExtensionType extension = new ExtensionType();
+        try {
+            extension.set_any(new MessageElement[] {new MessageElement(XMLUtils.newDocument().createElement(description))});
+        } catch (Exception e) {
+            throw new java.rmi.RemoteException(e.getMessage());
+        }
+        throw new ExtensionFault(extension);
+    }
+
 }
