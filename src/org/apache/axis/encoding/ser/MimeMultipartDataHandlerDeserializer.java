1e768274ea81102adb405a8598d224a428728e67
Davanum Srinivas <dims@apache.org>
2002-12-23 02:48:42 +0000

    Fix for Bug 15148 - test.wsdl.attachments test error is masked

--- src/org/apache/axis/encoding/ser/MimeMultipartDataHandlerDeserializer.java
+++ src/org/apache/axis/encoding/ser/MimeMultipartDataHandlerDeserializer.java
@@ -89,12 +89,7 @@ public class MimeMultipartDataHandlerDeserializer extends JAFDataHandlerDeserial
                 setValue(mmp);
             }
             catch (Exception e) {
-                // FIXME bug 15148 - we need to propigate these errors,
-                // but this breaks the test.wsdl/attachments multi-part tests,
-                // which pass empty stuff over the wire, which seems to screw
-                // up the MimeMultipart class.  Why did this ever work?
-                // throw new SAXException(e);
-                setValue(null);
+                throw new SAXException(e);
             }
         }
     } // startElement
