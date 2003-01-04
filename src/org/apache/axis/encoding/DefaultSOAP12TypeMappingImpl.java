e802d6be141a39082edd19f2efc47c16b208a7d7
James M. Snell <jmsnell@apache.org>
2003-01-04 01:34:24 +0000

    Adding DefaultSOAP12TypeMappingImpl back to fix backwards compatibility with Axis 1.0 DefaultSOAP12TypeMappingImpl extends DefaultSOAPEncodingTypeMappingImpl and carries the @depreciated tag

---
+++ src/org/apache/axis/encoding/DefaultSOAP12TypeMappingImpl.java
@@ -0,0 +1,15 @@
+package org.apache.axis.encoding;
+
+/**
+ * @author James M Snell <jasnell@us.ibm.com>
+ * @deprecated Please use DefaultSOAPEncodingTypeMappingImpl.java
+ * @see org.apache.axis.encoding.DefaultSOAPEncodingTypeMappingImpl 
+ */
+public class DefaultSOAP12TypeMappingImpl
+  extends DefaultSOAPEncodingTypeMappingImpl {
+  
+  public DefaultSOAP12TypeMappingImpl() {
+    super();
+  }
+
+}
