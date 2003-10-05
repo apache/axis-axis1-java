72e8bc4308175128f8503eed44297c3e9e9ea6b0
Steve Loughran <stevel@apache.org>
2003-10-05 07:10:02 +0000

    having got the package tests to run, they were failing all day with an error, that forced me to resort to hex dumps of strings. Turns out we were transposing \n and \r in the new (post Axis1.1) encoder.

--- test/encoding/EncodingTest.java
+++ test/encoding/EncodingTest.java
@@ -7,15 +7,17 @@ import org.apache.axis.components.encoding.XMLEncoderFactory;
 import java.io.UnsupportedEncodingException;
 
 /**
- * Tests for the new XMLEncoder components
+ * Tests for the new XMLEncoder components.
+ * Some of the tests are convoluted; that is to make diagnosis of faults easy, even
+ * with JUnit's XML reporting intervening in the process.
  */
 public class EncodingTest extends TestCase {
-    public static final String GERMAN_UMLAUTS = " Some text \u00df with \u00fc special \u00f6 chars \u00e4.";
-    public static final String XML_SPECIAL_CHARS = "< > \" &";
-    public static final String ENCODED_XML_SPECIAL_CHARS = "&lt; &gt; &quot; &amp;";
-    public static final String SUPPORT_CHARS_LESS_HEX_20 = "\t \r \n";
-    public static final String ENCODED_SUPPORT_CHARS_LESS_HEX_20 = "&#x9; &#xd; &#xa;";
-    public static final String INVALID_XML_STRING = "Invalid XML String \u0000";
+    private static final String GERMAN_UMLAUTS = " Some text \u00df with \u00fc special \u00f6 chars \u00e4.";
+    private static final String XML_SPECIAL_CHARS = "< > \" &";
+    private static final String ENCODED_XML_SPECIAL_CHARS = "&lt; &gt; &quot; &amp;";
+    private static final String SUPPORT_CHARS_LESS_HEX_20 = "\t\r\n";
+    private static final String ENCODED_SUPPORT_CHARS_LESS_HEX_20 = "&#x9;&#xd;&#xa;";
+    private static final String INVALID_XML_STRING = "Invalid XML String \u0000";
 
     public EncodingTest(String s) {
         super(s);
@@ -33,13 +35,12 @@ public class EncodingTest extends TestCase {
         for(int i=0;i<31;i++) {
             char c=(char)i;
             //ignore legit whitespace
-            if ("\t\n\r".indexOf(c) == 1) {
+            if ("\t\n\r".indexOf(c) == -1) {
                 //verify the others are caught
                 String s=(new Character(c)).toString();
                 assertInvalidStringsDetected(s);
             }
         }
-        assertInvalidStringsDetected("foo");
     }
 
     /**
@@ -72,29 +73,65 @@ public class EncodingTest extends TestCase {
     public void testUTF8() throws Exception {
         XMLEncoder encoder = XMLEncoderFactory.getEncoder(XMLEncoderFactory.ENCODING_UTF_8);
         String encodedUmlauts = encoder.encode(GERMAN_UMLAUTS);
-        String encodedXMLChars = encoder.encode(XML_SPECIAL_CHARS);
 
         assertEquals(XMLEncoderFactory.ENCODING_UTF_8, encoder.getEncoding());
-        assertEquals(ENCODED_XML_SPECIAL_CHARS, encodedXMLChars);
         assertEquals(GERMAN_UMLAUTS, new String(encodedUmlauts.getBytes(), XMLEncoderFactory.ENCODING_UTF_8));
-        //assert that the whitespace chars are not touched
-        assertEquals(SUPPORT_CHARS_LESS_HEX_20, encoder.encode(SUPPORT_CHARS_LESS_HEX_20));
+        verifyCommonAssertions(encoder);
     }
 
     public void testUTF16() throws Exception {
-        // this test needs to be improved, currently is does not test a lot
         XMLEncoder encoder = XMLEncoderFactory.getEncoder(XMLEncoderFactory.ENCODING_UTF_16);
         String encodedUmlauts = encoder.encode(GERMAN_UMLAUTS);
-        String encodedXMLChars = encoder.encode(XML_SPECIAL_CHARS);
 
         assertEquals(XMLEncoderFactory.ENCODING_UTF_16, encoder.getEncoding());
-        assertEquals(ENCODED_XML_SPECIAL_CHARS, encodedXMLChars);
         // java uses UTF-16 internally, should be equal
         assertEquals(GERMAN_UMLAUTS, encodedUmlauts);
-        //assert that the whitespace chars are not touched
-        assertEquals(SUPPORT_CHARS_LESS_HEX_20, encoder.encode(SUPPORT_CHARS_LESS_HEX_20));
+        verifyCommonAssertions(encoder);
     }
 
+
+    /**
+     * assertions here hold for either encoder
+     * @param encoder
+     */
+    private void verifyCommonAssertions(XMLEncoder encoder) {
+        String encodedXMLChars = encoder.encode(XML_SPECIAL_CHARS);
+        assertEquals(ENCODED_XML_SPECIAL_CHARS, encodedXMLChars);
+        //assert that the whitespace chars are not touched
+        verifyUntouched(encoder, "\t");
+        verifyUntouched(encoder, "\n");
+        verifyUntouched(encoder, "\r");
+    }
+
+    /**
+     * verify that the support chars are not touched. This is done on a char by
+     * char basis for easier debugging. One debug problem there is that
+     * ant's XML logger also encodes the strings, making diagnosing
+     * the defect from an error report trickier than normal.
+     * @param encoder
+     */
+    private void verifyUntouched(XMLEncoder encoder, String source) {
+        for(int i=0;i<source.length();i++) {
+            char c = source.charAt(i);
+            Character ch = new Character(c);
+            String xmlString = ch.toString();
+            String encoded= encoder.encode(xmlString);
+            assertEquals("Char " +(int) c + " was encoded as " + hexDump(encoded),
+                    xmlString,encoded);
+        }
+    }
+
+    private String hexDump(String source) {
+        StringBuffer out=new StringBuffer(source.length()*5);
+        for (int i = 0; i < source.length(); i++) {
+            char c = source.charAt(i);
+            out.append("0x");
+            out.append(Integer.toHexString(c));
+            out.append(" ");
+        }
+        return new String(out);
+
+    }
     public static void main(String[] args) {
         junit.textui.TestRunner.run(EncodingTest.class);
     }
