4edb3accb60644dcae1b376fb6b5a0650c00903d
Davanum Srinivas <dims@apache.org>
2003-10-13 18:02:05 +0000

    Fix for Bug 23712 - NPE in BeanUtils.processPropertyDescriptors

--- src/org/apache/axis/utils/BeanUtils.java
+++ src/org/apache/axis/utils/BeanUtils.java
@@ -121,7 +121,7 @@ public class BeanUtils {
                                 result = Introspector.
                                         getBeanInfo(secJavaType,AxisFault.class).
                                         getPropertyDescriptors();
-                            } else if (Throwable.class.isAssignableFrom(secJavaType)) {
+                            } else if (Throwable.class != secJavaType && Throwable.class.isAssignableFrom(secJavaType)) {
                                 // Don't include Throwable data
                                 result = Introspector.
                                         getBeanInfo(secJavaType,Throwable.class).
