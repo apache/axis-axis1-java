17063d98a11f8b4e14092dfa3b91492da7c14bf6
Davanum Srinivas <dims@apache.org>
2003-11-28 03:57:51 +0000

    Fix for Bug 23031 - No deserializer found for ns1:ArrayOfstring from zogthebogue@hotmail.com (Zog TheBogue)

---
+++ test/client/PackageTests.java
@@ -0,0 +1,21 @@
+package test.client;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ */
+public class PackageTests
+{
+    public static void main (String[] args) {
+            junit.textui.TestRunner.run (suite());
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("All Axis Call tests");
+
+        suite.addTest(TestCall.suite());
+        return suite;
+    }
+}
