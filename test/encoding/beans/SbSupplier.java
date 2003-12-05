50285c2a90d632af463ccf13b394562833199b5e
Davanum Srinivas <dims@apache.org>
2003-12-05 16:33:29 +0000

    test case for Bug 25179 - BeanDeserializer cannot deserialize child array unless xsi:type="SE:Array" provided
    Notes: Bug has been already fixed in Axis 1.2 Alpha.

--- test/encoding/beans/SbSupplier.java
+++ test/encoding/beans/SbSupplier.java
@@ -6,4 +6,5 @@ public class SbSupplier implements java.io.Serializable
     public Integer searchType;
     public String  supplierCode;
     public String  chanNel;
+    public String[]  listOfStrings;
 }
