d43f8a5c471dd5d1ed1d5ce2adc67eaa36458f3a
Davanum Srinivas <dims@apache.org>
2003-12-08 16:03:06 +0000

    Fix for Bug 25179 - BeanDeserializer cannot deserialize child array unless xsi:type="SE:Array" provided
    Notes:
    - Make sure that we run the test case.

--- test/encoding/PackageTests.java
+++ test/encoding/PackageTests.java
@@ -35,6 +35,7 @@ public class PackageTests extends TestCase
         suite.addTestSuite(TestAttributes.class);
         suite.addTestSuite(TestBeanDeser.class);
         suite.addTestSuite(TestBeanDeser2.class);
+        suite.addTestSuite(TestBeanDeser3.class);
         suite.addTestSuite(TestRoundTrip.class);
         suite.addTestSuite(TestOmittedValues.class);
         suite.addTestSuite(TestMultiRefIdentity.class);
