3a37ab182e604d6a18fa0e03894a61f0ff30b659
Davanum Srinivas <dims@apache.org>
2003-12-09 21:21:39 +0000

    Add try/catch and timeout.

--- test/client/TestCall.java
+++ test/client/TestCall.java
@@ -63,6 +63,8 @@ import org.apache.axis.client.Call;
 import org.apache.axis.handlers.BasicHandler;
 
 import java.net.URL;
+import java.net.ConnectException;
+import java.io.InterruptedIOException;
 
 public class TestCall extends TestCase {
     public TestCall(String name) {
@@ -77,15 +79,33 @@ public class TestCall extends TestCase {
     }
 
     /* Test case for Bug 23031 - No deserializer found for ns1:ArrayOfstring */
-    public void testWeatherService() throws Exception {
-        Call call = new Call(new URL("http://live.capescience.com:80/ccx/GlobalWeather"));
-        call.setUseSOAPAction(true);
-        call.setSOAPActionURI("capeconnect:GlobalWeather:StationInfo#listCountries");
-        call.setOperationName(new javax.xml.namespace.QName("capeconnect:GlobalWeather:StationInfo", "listCountries"));
-        String[] c = (String[]) call.invoke(new Object[]{});
-        System.out.println(c.length);
-        for (int i = 0; i < c.length; i++) {
-            System.out.println(c[i]);
+    public void testWeatherService() {
+        try {
+            Call call = new Call(new URL("http://live.capescience.com:80/ccx/GlobalWeather"));
+            call.setUseSOAPAction(true);
+            call.setSOAPActionURI("capeconnect:GlobalWeather:StationInfo#listCountries");
+            call.setTimeout(new Integer(15*1000));
+            call.setOperationName(new javax.xml.namespace.QName("capeconnect:GlobalWeather:StationInfo", "listCountries"));
+            String[] c = (String[]) call.invoke(new Object[]{});
+            System.out.println(c.length);
+            for (int i = 0; i < c.length; i++) {
+                System.out.println(c[i]);
+            }
+        }  catch (java.rmi.RemoteException re) {
+            if (re instanceof AxisFault) {
+                AxisFault fault = (AxisFault) re;
+                if (fault.detail instanceof ConnectException ||
+                    fault.detail instanceof InterruptedIOException ||
+                    (fault.getFaultString().indexOf("Connection timed out") != -1) ||
+                    fault.getFaultCode().getLocalPart().equals("HTTP")) {
+                    System.err.println("getTemp HTTP error: " + fault);
+                    return;
+                }
+            }
+            throw new junit.framework.AssertionFailedError("Remote Exception caught: " + re);
+        }  catch (java.io.IOException ioe) {
+            System.err.println("testWeatherService connect error: " + ioe);
+            return;
         }
     }
 }
