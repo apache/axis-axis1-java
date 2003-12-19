3eb26756556d58a19516a74c038f425e09e2935b
Changshin Lee <ias@apache.org>
2003-12-19 09:08:01 +0000

    Fix for Bug 25558 - Mapped aThing parameter to String

--- test/wsdl/wrapped_holders/ThingImpl.java
+++ test/wsdl/wrapped_holders/ThingImpl.java
@@ -16,12 +16,12 @@ package test.wsdl.wrapped_holders;
  * @author Tom Jordahl
  */ 
 public class ThingImpl implements test.wsdl.wrapped_holders.Thing{
-    public void find_aThing(test.wsdl.wrapped_holders.AThing aThing, 
+    public void find_aThing(String aThing, 
                             test.wsdl.wrapped_holders.holders.AThingArrayHolder aThingUnbounded, 
                             test.wsdl.wrapped_holders.holders.OtherDataArrayHolder otherDataUnbounded) 
             throws java.rmi.RemoteException {
         // Verify we get a string in aThing input argument
-        String input = aThing.getValue();
+        String input = aThing;
         if (input == null || !input.equals("This is a test")) {
             String error = "Input argument did not match expected string, got: ";
             error += input != null ? "'" + input + "'" : "NULL";
@@ -30,13 +30,13 @@ public class ThingImpl implements test.wsdl.wrapped_holders.Thing{
         }
         
         // now send something back
-        test.wsdl.wrapped_holders.AThing[] things = new test.wsdl.wrapped_holders.AThing[2];
-        things[0] = new AThing("Thing one");
-        things[1] = new AThing("Thing two");
+        String[] things = new String[2];
+        things[0] = new String("Thing one");
+        things[1] = new String("Thing two");
         aThingUnbounded.value = things;
-        test.wsdl.wrapped_holders.OtherData[] others = new test.wsdl.wrapped_holders.OtherData[2];
-        others[0] = new OtherData("Other 1");
-        others[1] = new OtherData("Other 2");
+        String[] others = new String[2];
+        others[0] = new String("Other 1");
+        others[1] = new String("Other 2");
         otherDataUnbounded.value = others;
     }
 
