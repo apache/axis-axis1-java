ece7eac2a7c354e189eb2e6faf33f2255533239a
Davanum Srinivas <dims@apache.org>
2005-04-29 18:48:16 +0000

    As promised the test case for replicating jonas failures.

---
+++ test/wsdl/marshall3/MarshallTestServiceTestCase.java
@@ -0,0 +1,88 @@
+/**
+ * MarshallTestServiceTestCase.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC3 Apr 29, 2005 (10:05:23 EDT) WSDL2Java emitter.
+ */
+
+package test.wsdl.marshall3;
+
+import test.wsdl.marshall3.types.ShortArrayTest;
+import test.wsdl.marshall3.types.ShortArrayTestResponse;
+import test.wsdl.marshall3.types.StringArrayTest;
+import test.wsdl.marshall3.types.StringArrayTestResponse;
+
+public class MarshallTestServiceTestCase extends junit.framework.TestCase {
+    public MarshallTestServiceTestCase(java.lang.String name) {
+        super(name);
+    }
+
+    public void test1Marshall3TestPort1ShortArrayTest() throws Exception {
+        test.wsdl.marshall3.Marshall3TestPort1SoapBindingStub binding;
+        try {
+            binding = (test.wsdl.marshall3.Marshall3TestPort1SoapBindingStub)
+                          new test.wsdl.marshall3.MarshallTestServiceLocator().getMarshall3TestPort1();
+        }
+        catch (javax.xml.rpc.ServiceException jre) {
+            if(jre.getLinkedCause()!=null)
+                jre.getLinkedCause().printStackTrace();
+            throw new junit.framework.AssertionFailedError("JAX-RPC ServiceException caught: " + jre);
+        }
+        assertNotNull("binding is null", binding);
+
+        // Time out after a minute
+        binding.setTimeout(60000);
+
+        // Test operation
+        ShortArrayTestResponse value = null;
+        value = binding.shortArrayTest(new ShortArrayTest(new short[]{1,2,3}));
+        assertEquals(3,value.getShortArray().length);
+    }
+
+    public void test2Marshall3TestPort1StringArrayTest() throws Exception {
+        test.wsdl.marshall3.Marshall3TestPort1SoapBindingStub binding;
+        try {
+            binding = (test.wsdl.marshall3.Marshall3TestPort1SoapBindingStub)
+                          new test.wsdl.marshall3.MarshallTestServiceLocator().getMarshall3TestPort1();
+        }
+        catch (javax.xml.rpc.ServiceException jre) {
+            if(jre.getLinkedCause()!=null)
+                jre.getLinkedCause().printStackTrace();
+            throw new junit.framework.AssertionFailedError("JAX-RPC ServiceException caught: " + jre);
+        }
+        assertNotNull("binding is null", binding);
+
+        // Time out after a minute
+        binding.setTimeout(60000);
+
+        // Test operation
+        StringArrayTestResponse value = null;
+        value = binding.stringArrayTest(new StringArrayTest(new String[]{"1","2","","4",null,"6"}));
+        // TBD - validate results
+        assertEquals(6,value.getStringArray().length);
+    }
+
+    public void test3Marshall3TestPort1EchoShortListTypeTest() throws Exception {
+        test.wsdl.marshall3.Marshall3TestPort1SoapBindingStub binding;
+        try {
+            binding = (test.wsdl.marshall3.Marshall3TestPort1SoapBindingStub)
+                          new test.wsdl.marshall3.MarshallTestServiceLocator().getMarshall3TestPort1();
+        }
+        catch (javax.xml.rpc.ServiceException jre) {
+            if(jre.getLinkedCause()!=null)
+                jre.getLinkedCause().printStackTrace();
+            throw new junit.framework.AssertionFailedError("JAX-RPC ServiceException caught: " + jre);
+        }
+        assertNotNull("binding is null", binding);
+
+        // Time out after a minute
+        binding.setTimeout(60000);
+
+        // Test operation
+        short[] value = null;
+        value = binding.echoShortListTypeTest(new short[]{1,2,3});
+        // TBD - validate results
+        assertEquals(3,value.length);
+    }
+
+}
