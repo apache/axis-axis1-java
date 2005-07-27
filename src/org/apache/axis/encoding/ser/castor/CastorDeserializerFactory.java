a4a27e90b2e552f0ac355a76fd623ab4fb2f28ee
Davanum Srinivas <dims@apache.org>
2005-07-27 04:01:46 +0000

    oops! fix imports.

--- src/org/apache/axis/encoding/ser/castor/CastorDeserializerFactory.java
+++ src/org/apache/axis/encoding/ser/castor/CastorDeserializerFactory.java
@@ -17,6 +17,7 @@
 package org.apache.axis.encoding.ser.castor;
 
 import org.apache.axis.encoding.ser.BaseDeserializerFactory;
+import org.apache.axis.encoding.DeserializerFactory;
 
 import javax.xml.namespace.QName;
 
--- src/org/apache/axis/encoding/ser/castor/CastorEnumTypeSerializerFactory.java
+++ src/org/apache/axis/encoding/ser/castor/CastorEnumTypeSerializerFactory.java
@@ -17,6 +17,7 @@
 package org.apache.axis.encoding.ser.castor;
 
 import org.apache.axis.encoding.ser.BaseSerializerFactory;
+import org.apache.axis.encoding.SerializerFactory;
 
 import javax.xml.namespace.QName;
 
