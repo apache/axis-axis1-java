7a67b9611104bc29c2294d84b69349a7808e144e
Davanum Srinivas <dims@apache.org>
2005-11-03 04:23:06 +0000

    oops!

--- src/org/apache/axis/utils/JavaUtils.java
+++ src/org/apache/axis/utils/JavaUtils.java
@@ -43,6 +43,7 @@ import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.WeakHashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
