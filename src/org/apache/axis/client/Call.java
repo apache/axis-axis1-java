ddad1319a513eda7331c8b7a1db9ae526fb6cd57
Jochen Wiedmann <jochen@apache.org>
2008-11-04 07:21:28 +0000

    Temporary files could be deleted too early, preventing their further processing. PR: AXIS-2574 Submitted-By: Rodrigo Ruiz <rruiz@gridsystems.com>

--- src/org/apache/axis/client/Call.java
+++ src/org/apache/axis/client/Call.java
@@ -95,6 +95,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.Vector;
+import java.util.WeakHashMap;
 import java.rmi.RemoteException;
 
 import org.apache.axis.wsa.WSAConstants ;
