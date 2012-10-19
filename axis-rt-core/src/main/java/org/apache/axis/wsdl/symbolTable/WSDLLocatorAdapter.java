7946f75901a22248bbd5fc0aa0e8e71b45502a2f
Andreas Veithen <veithen@apache.org>
2012-10-19 09:12:55 +0000

    Ensure that WSDLs that import the http://www.w3.org/XML/1998/namespace and http://schemas.xmlsoap.org/soap/encoding/ namespaces and that specify a schemaLocation for these namespaces can be processed successfully without network connection.

--- axis-rt-core/src/main/java/org/apache/axis/wsdl/symbolTable/WSDLLocatorAdapter.java
+++ axis-rt-core/src/main/java/org/apache/axis/wsdl/symbolTable/WSDLLocatorAdapter.java
@@ -21,6 +21,9 @@ package org.apache.axis.wsdl.symbolTable;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.wsdl.xml.WSDLLocator;
 
@@ -29,23 +32,52 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 class WSDLLocatorAdapter implements WSDLLocator {
+    /**
+     * Contains a set of schemas that are always resolved locally. {@link SymbolTable} actually
+     * doesn't need to read these schemas, and it will not read them if they are imported without
+     * specifying a <tt>schemaLocation</tt> (see {@link SymbolTable#isKnownNamespace(String)}).
+     * However, WSDL4J insists on loading them whenever they are references by a
+     * <tt>schemaLocation</tt>.
+     */
+    private final static Map/*<String,URL>*/ localSchemas;
+    
     private final String baseURI;
     private final EntityResolver entityResolver;
     private String latestImportURI;
 
+    static {
+        localSchemas = new HashMap();
+        localSchemas.put("http://www.w3.org/2001/xml.xsd",
+                WSDLLocatorAdapter.class.getResource("xml.xsd"));
+        localSchemas.put("http://schemas.xmlsoap.org/soap/encoding/",
+                WSDLLocatorAdapter.class.getResource("soap-encoding.xsd"));
+    }
+    
     WSDLLocatorAdapter(String baseURI, EntityResolver entityResolver) {
         this.baseURI = baseURI;
         this.entityResolver = entityResolver;
     }
 
     private InputSource getInputSource(String uri) {
-        try {
-            InputSource is = entityResolver.resolveEntity(null, uri);
-            return is != null ? is : new InputSource(uri);
-        } catch (SAXException ex) {
-            return null;
-        } catch (IOException ex) {
-            return null;
+        URL localSchema = (URL)localSchemas.get(uri);
+        if (localSchema != null) {
+            InputSource is;
+            try {
+                is = new InputSource(localSchema.openStream());
+            } catch (IOException ex) {
+                return null;
+            }
+            is.setSystemId(uri);
+            return is;
+        } else {
+            try {
+                InputSource is = entityResolver.resolveEntity(null, uri);
+                return is != null ? is : new InputSource(uri);
+            } catch (SAXException ex) {
+                return null;
+            } catch (IOException ex) {
+                return null;
+            }
         }
     }
     
