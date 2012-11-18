23a1c1aa9db764761e357500eb5c13561c6c401c
Andreas Veithen <veithen@apache.org>
2012-11-18 09:10:01 +0000

    Avoid using hardcoded port numbers in unit tests (to ensure that the build can be run concurrently with other builds).

--- samples/mtomstub-sample/src/test/java/samples/mtomstub/TestDownloadFile.java
+++ samples/mtomstub-sample/src/test/java/samples/mtomstub/TestDownloadFile.java
@@ -4,6 +4,8 @@ import java.net.URL;
 
 import javax.xml.ws.Endpoint;
 
+import org.apache.axiom.testutils.PortAllocator;
+
 import junit.framework.TestCase;
 import samples.mtomstub.service.DownloadFileImpl;
 import samples.mtomstub.stub.DownloadFile;
@@ -11,8 +13,7 @@ import samples.mtomstub.stub.DownloadFileServiceLocator;
 
 public class TestDownloadFile extends TestCase {
     public void test() throws Exception {
-        // TODO: allocate random port here
-        String url = "http://localhost:8080/DownloadFile";
+        String url = "http://localhost:" + PortAllocator.allocatePort() + "/DownloadFile";
         Endpoint endpoint = Endpoint.publish(url, new DownloadFileImpl());
         DownloadFile downloadFile = new DownloadFileServiceLocator().getDownloadFilePort(new URL(url));
         downloadFile.getFile().getFile().writeTo(System.out);
